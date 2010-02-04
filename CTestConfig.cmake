b1c0009ce20a6cef2e74890d644b85f0f66ec3ad
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-02-04 12:54:48 +0000

    trying to set up dashboard

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "OpenMEEG")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "cdash.inria.fr")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=OpenMEEG")
+set(CTEST_DROP_SITE_CDASH TRUE)
