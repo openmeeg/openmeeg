5d8935c73fbf8767c0a91556d3023242b1dbbf24
Theodore Papadopoulo <theodore.papadopoulo@inria.fr>
2010-03-08 21:52:30 +0000

    Correct wrong absolute path.

--- macros/FortranCInterface.cmake
+++ macros/FortranCInterface.cmake
@@ -134,7 +134,7 @@ function(FortranCInterface_HEADER file)
   endforeach()
 
   #set(FortranCInterface_SOURCE_DIR ${CMAKE_ROOT}/Modules/FortranCInterface)
-  set(FortranCInterface_SOURCE_DIR /user/papadop/home/tmp/cmake-fortran/cmake/FortranCInterface)
+  set(FortranCInterface_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/macros/FortranCInterface)
 
   # Create the interface detection project if it does not exist.
   if(NOT FortranCInterface_BINARY_DIR)
