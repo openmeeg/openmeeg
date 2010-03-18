8aa33c8ce9f174bc73e527c351b4fd07cbbed95b
Emmanuel Olivi <emmanuel.olivi@inria.fr>
2010-03-18 14:12:51 +0000

    Removing unnecessary header comments.

--- src/mesh3.h
+++ src/mesh3.h
@@ -3,11 +3,6 @@
 /*
 Project Name : OpenMEEG
 
-version           : $Revision$
-last revision     : $Date$
-modified by       : $LastChangedBy$
-last modified     : $LastChangedDate$
-
 © INRIA and ENPC (contributors: Geoffray ADDE, Maureen CLERC, Alexandre
 GRAMFORT, Renaud KERIVEN, Jan KYBIC, Perrine LANDREAU, Théodore PAPADOPOULO,
 Maureen.Clerc.AT.sophia.inria.fr, keriven.AT.certis.enpc.fr,
