4c4211d72839f2b1feb15fa5242d13c70419fdd9
Theodore Papadopoulo <theodore.papadopoulo@inria.fr>
2010-03-25 15:27:36 +0000

    Change the logic and add an option to use the installed matio libraries.

--- macros/UseMatio.cmake
+++ macros/UseMatio.cmake
@@ -2,38 +2,27 @@
 # MATIO library
 #------------------------------------------------------------
 
-OPTION(USE_MATIO "Build the project using MATIO" OFF)
+OPTION(USE_SYSTEM_MATIO "Build the project using an already installed MATIO" OFF)
 
-IF (USE_MATIO)
+IF (USE_SYSTEM_MATIO)
+    FIND_LIBRARY(MATIO_LIBRARIES matio)
+ENDIF()
 
+IF (NOT MATIO_LIBRARIES)
     INCLUDE_DIRECTORIES(${OpenMEEG_SOURCE_DIR}/contrib/matio/src)
     INCLUDE_DIRECTORIES(${OpenMEEG_SOURCE_DIR}/contrib/matio/zlib)
     INCLUDE_DIRECTORIES(${OpenMEEG_BINARY_DIR}/contrib/matio/src)
     INCLUDE_DIRECTORIES(${OpenMEEG_BINARY_DIR}/contrib/matio/zlib)
     
     IF(WIN32) 
-	INCLUDE_DIRECTORIES(${OpenMEEG_SOURCE_DIR}/contrib/matio/contrib/Windows)
+        INCLUDE_DIRECTORIES(${OpenMEEG_SOURCE_DIR}/contrib/matio/contrib/Windows)
     ENDIF()
 
     SET(MATIO_LIBRARIES matio)
+ENDIF()
 
-    # SET(SEARCHPATH
-    #     /usr/lib/
-    #     /usr/lib64/
-    #     /usr/local/lib/atlas
-    #     $ENV{HOME}/local/lib
-    # )
-    # 
-    # FIND_LIBRARY(MATIO_LIB NAMES matio PATHS ${SEARCHPATH})
-    # FIND_LIBRARY(ZLIB_LIB NAMES z PATHS ${SEARCHPATH})
-    # 
-    # SET(MATIO_LIBRARIES ${MATIO_LIB} ${ZLIB_LIB})
-    # 
-    # FIND_PATH(MATIO_INCLUDE_PATH matio.h
-    #           /usr/include/
-    #           /usr/local/include/
-    #           $ENV{HOME}/local/include/
-    # )
-    # INCLUDE_DIRECTORIES(${MATIO_INCLUDE_PATH})
-
+IF (NOT MATIO_LIBRARIES)
+    MESSAGE(ERROR "There is a problem detecting Matio")
+ELSE()
+    SET(USE_MATIO 1)
 ENDIF()
