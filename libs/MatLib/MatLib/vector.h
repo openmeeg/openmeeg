0c9519aa8384517be8e9e35f934fe45f720b881a
Theodore Papadopoulo <theodore.papadopoulo@inria.fr>
2010-03-26 16:48:05 +0000

    Remove definitely svn's Id and co which prevented easy file comparisons.

--- libs/MatLib/MatLib/vector.h
+++ libs/MatLib/MatLib/vector.h
@@ -1,12 +1,5 @@
-/* FILE: $Id$ */
-
 /*
 Project Name : OpenMEEG
-
-version           : $Revision$
-last revision     : $Date$
-modified by       : $LastChangedBy$
-last modified     : $LastChangedDate$
 
 © INRIA and ENPC (contributors: Geoffray ADDE, Maureen CLERC, Alexandre
 GRAMFORT, Renaud KERIVEN, Jan KYBIC, Perrine LANDREAU, Théodore PAPADOPOULO,
@@ -63,10 +56,10 @@ class OPENMEEGMATHS_EXPORT Vector: public LinOp {
 
 public:
 
-    Vector(): LinOp(0,1,FULL,ONE),value() { }
+    Vector(): LinOp(0,1,FULL,1),value() { }
 
-    Vector(const size_t N): LinOp(N,1,FULL,ONE),value(new LinOpValue(size())) { }
-    Vector(const Vector& A,const DeepCopy): LinOp(A.nlin(),1,FULL,ONE),value(new LinOpValue(A.size(),A.data())) { }
+    Vector(const size_t N): LinOp(N,1,FULL,1),value(new LinOpValue(size())) { }
+    Vector(const Vector& A,const DeepCopy): LinOp(A.nlin(),1,FULL,1),value(new LinOpValue(A.size(),A.data())) { }
 
     explicit Vector(Matrix& A);
     explicit Vector(SymMatrix& A);
