1777103917dc013279542c68d340f5006d0785e4
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-03-27 16:57:38 +0000

    adding Emmanuel Olivi to authors list

--- src/assemble.h
+++ src/assemble.h
@@ -3,6 +3,7 @@ Project Name: OpenMEEG
 
 © INRIA and ENPC (contributors: Geoffray ADDE, Maureen CLERC, Alexandre 
 GRAMFORT, Renaud KERIVEN, Jan KYBIC, Perrine LANDREAU, Théodore PAPADOPOULO,
+Emmanuel OLIVI
 Maureen.Clerc.AT.sophia.inria.fr, keriven.AT.certis.enpc.fr,
 kybic.AT.fel.cvut.cz, papadop.AT.sophia.inria.fr)
 
