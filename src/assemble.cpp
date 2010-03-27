b239984e0beb3108e3116ed5ee122322f2d06eb4
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-03-27 16:57:38 +0000

    adding Emmanuel Olivi to authors list

--- src/assemble.cpp
+++ src/assemble.cpp
@@ -2,6 +2,7 @@
 
 © INRIA and ENPC (contributors: Geoffray ADDE, Maureen CLERC, Alexandre
 GRAMFORT, Renaud KERIVEN, Jan KYBIC, Perrine LANDREAU, Théodore PAPADOPOULO,
+Emmanuel OLIVI
 Maureen.Clerc.AT.sophia.inria.fr, keriven.AT.certis.enpc.fr,
 kybic.AT.fel.cvut.cz, papadop.AT.sophia.inria.fr)
 
