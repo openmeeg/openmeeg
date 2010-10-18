f8ae12083f7a6a864e8e7c0941f755d7b0e61a41
Emmanuel Olivi <emmanuel.olivi@inria.fr>
2010-10-18 09:49:17 +0000

    adding forgotten include in gain.h

--- src/gain.h
+++ src/gain.h
@@ -45,6 +45,7 @@ knowledge of the CeCILL-B license and that you accept its terms.
 #include "matrix.h"
 #include "sparse_matrix.h"
 #include "symmatrix.h"
+#include "matvectOps.h"
 #include "inversers.h"
 #include "geometry.h"
 #include "assemble.h"
