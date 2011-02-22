794fb3c30e175ddeaa2d828a03b4f6ef5b9760e2
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-02-22 18:55:23 +0000

    adding nightly build scripts

---
+++ pipol/openmeeg_nightly.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#PIPOL esn amd64_kvm-linux-debian-lenny  none 2:00 --silent --root
+#PIPOL esn amd64-linux-fedora-core11.dd.gz none 2:00 --silent --root
+#PIPOL esn i386_kvm-linux-debian-lenny  none 2:00 --silent --root
+#PIPOL esn i386-linux-fedora-core11.dd.gz none 2:00 --silent --root
+#PIPOL esn mac-osx-server-snow-leopard none 0:40 --silent --root
+
+SYSTEM=`uname`
+ARCH=`uname -m`
+
+if [ -e ./pipol ] ; then
+	rm -rf ./pipol/$PIPOL_HOST
+	mkdir ./pipol/$PIPOL_HOST
+else
+	mkdir ./pipol
+	rm -rf ./pipol/$PIPOL_HOST
+	mkdir ./pipol/$PIPOL_HOST
+fi
+cd ./pipol/$PIPOL_HOST
+
+svn checkout svn://scm.gforge.inria.fr/svn/openmeeg/trunk openmeeg-trunk --quiet
+
+sh ./openmeeg-trunk/pipol/install_packages.sh
+perl ./openmeeg-trunk/pipol/cmake.pl
+
+cd openmeeg-trunk
+
+# Handle MKL
+if [ x$SYSTEM = xDarwin ] ; then
+    export MKLDIR=/netshare/i386_mac/icc/11.0.074/Frameworks/mkl/ # mac 32
+fi
+if [ x$SYSTEM = xLinux ] ; then
+    if [ x$ARCH = xx86_64 ]; then
+        export MKLDIR=/netshare/amd64/icc/11.0.074/mkl/ # linux 64
+    else
+        export MKLDIR=/netshare/i386/icc/11.0.074/mkl/ # linux 32
+    fi
+fi
+
+function build {
+    make NightlyUpdate
+    make NightlyConfigure
+    make NightlyBuild
+    make NightlyTest
+    if [ x$SYSTEM = xLinux ] ; then
+        make NightlyCoverage
+        make NightlyMemCheck
+    fi
+    make NightlySubmit
+    make clean
+}
+
+
+if [ x$SYSTEM = xLinux ] ; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DENABLE_PACKAGING=ON -DENABLE_PYTHON=ON -DUSE_PROGRESSBAR=ON -DENABLE_COVERAGE=ON .
+fi
+
+if [ x$SYSTEM = xDarwin ] ; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DENABLE_PACKAGING=ON -DUSE_PROGRESSBAR=ON -DENABLE_COVERAGE=ON .
+fi
+
+build
+
+## MKL (static mode)
+#rm -f CMakeCache.txt
+#cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON -DENABLE_PACKAGING=ON -DUSE_ATLAS=OFF -DUSE_MKL=ON -DBUILD_SHARED=OFF -DUSE_PROGRESSBAR=ON .
+#build
+
---
+++ pipol/upload_nightly.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# script to update the openmeeg nightly build config on pipol
+
+scp openmeeg_nightly.sh pipol:~/.pipol/nightly/
