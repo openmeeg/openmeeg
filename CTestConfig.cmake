b41340a0ee0dfc3341e98497bb8111569877ffad
Theodore Papadopoulo <theodore.papadopoulo@inria.fr>
2011-06-28 11:37:52 +0000

    Revert change that prevents dashboard to work correctly.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,9 @@
 set(CTEST_PROJECT_NAME "OpenMEEG")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
-set(CTEST_DROP_METHOD "https")
+#Does not work currently.
+#set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.inria.fr")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=openMEEG")
 set(CTEST_DROP_SITE_CDASH TRUE)
