f9986f6f46becdf75c51dfb4f18d903992a45647
Theodore Papadopoulo <theodore.papadopoulo@inria.fr>
2011-07-28 15:33:39 +0000

    First try to factorize pipol scripts (only done for experimental so far so no real factorization yet.... Comments ?

--- pipol/deploy_windows.sh
+++ pipol/deploy_windows.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-scp experimental_windows.sh pipol:.
+file=`mktemp`
+cp openmeeg_windows.sh $file
+cat >> $file <<EOF
+build_and_test "Experimental"
+EOF
+
+chmod +x $file
+scp $file pipol:experimental_windows.sh
+rm $file
 
 # win xp 32bits
 ssh pipol pipol-sub esn i386_kvm-windows-xp-pro-sp3 none 01:00 "bash ~/experimental_windows.sh"
