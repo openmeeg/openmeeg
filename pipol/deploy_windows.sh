8861dab979c725cec522f7eb94b044114560bcdc
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-07-28 13:20:52 +0000

    increase windows VM time on pipol

--- pipol/deploy_windows.sh
+++ pipol/deploy_windows.sh
@@ -3,7 +3,7 @@
 scp experimental_windows.sh pipol:.
 
 # win xp 32bits
-ssh pipol pipol-sub esn i386_kvm-windows-xp-pro-sp3 none 00:40 "bash ~/experimental_windows.sh"
+ssh pipol pipol-sub esn i386_kvm-windows-xp-pro-sp3 none 01:00 "bash ~/experimental_windows.sh"
 
 # win 7 64bits
-ssh pipol pipol-sub esn amd64_kvm-windows-7 none 00:40 "bash ~/experimental_windows.sh"
+ssh pipol pipol-sub esn amd64_kvm-windows-7 none 01:00 "bash ~/experimental_windows.sh"
