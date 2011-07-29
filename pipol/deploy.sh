32d6f94a1def29b1b10771a0fc16e94dacf47587
Theodore Papadopoulo <theodore.papadopoulo@inria.fr>
2011-07-29 16:12:55 +0000

    Simplify somewhat pipol scripts.

--- pipol/deploy.sh
+++ pipol/deploy.sh
@@ -2,26 +2,49 @@
 
 scp experimental.sh pipol:.
 
-ssh pipol pipol-sub esn i386-linux-ubuntu-intrepid.dd.gz none 02:00 "~/experimental.sh"
-ssh pipol pipol-sub esn amd64-linux-ubuntu-intrepid.dd.gz none 02:00 "~/experimental.sh"
-
-ssh pipol pipol-sub esn i386-linux-ubuntu-jaunty.dd.gz none 02:00 "~/experimental.sh"
-ssh pipol pipol-sub esn amd64-linux-ubuntu-jaunty.dd.gz none 02:00 "~/experimental.sh"
-
-ssh pipol pipol-sub esn i386-linux-ubuntu-karmic.dd.gz none 02:00 "~/experimental.sh"
-ssh pipol pipol-sub esn amd64-linux-ubuntu-karmic.dd.gz none 02:00 "~/experimental.sh"
-
+# linux 32
+ssh pipol pipol-sub esn i386_kvm-linux-debian-lenny none 02:00 "~/experimental.sh"
 ssh pipol pipol-sub esn i386-linux-fedora-core11.dd.gz none 02:00 "~/experimental.sh"
+
+# linux 64
+ssh pipol pipol-sub esn amd64_kvm-linux-debian-lenny none 02:00 "~/experimental.sh"
 ssh pipol pipol-sub esn amd64-linux-fedora-core11.dd.gz none 02:00 "~/experimental.sh"
 
-ssh pipol pipol-sub esn i386_kvm-linux-debian-lenny none 02:00 "~/experimental.sh"
-ssh pipol pipol-sub esn i386_kvm-linux-debian-testing none 02:00 "~/experimental.sh"
+# mac 32
+ssh pipol pipol-sub esn i386_mac-mac-osx-server-leopard none 02:00 "~/experimental.sh"
 
-ssh pipol pipol-sub esn amd64_kvm-linux-debian-lenny none 02:00 "~/experimental.sh"
-ssh pipol pipol-sub esn amd64_kvm-linux-debian-testing none 02:00 "~/experimental.sh"
+# mac 64
+#ssh pipol pipol-sub esn x86_64_mac-mac-osx-server-snow-leopard none 02:00 "~/experimental.sh"
 
-ssh pipol pipol-sub esn x86_64_mac-mac-osx-server-snow-leopard none 02:00 "~/experimental.sh"
-# ssh pipol pipol-sub esn x86_mac-mac-osx-server-snow-leopard none 02:00 "~/experimental.sh"
+if [ x$1 != "x-simple" ]; then
 
-ssh pipol pipol-sub esn i386-linux-centos-5 none 02:00 "~/experimental.sh"
-ssh pipol pipol-sub esn amd64-linux-centos-5 none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn i386-linux-ubuntu-intrepid.dd.gz none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn amd64-linux-ubuntu-intrepid.dd.gz none 02:00 "~/experimental.sh"
+
+    ssh pipol pipol-sub esn i386-linux-ubuntu-jaunty.dd.gz none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn amd64-linux-ubuntu-jaunty.dd.gz none 02:00 "~/experimental.sh"
+
+    ssh pipol pipol-sub esn i386-linux-ubuntu-karmic.dd.gz none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn amd64-linux-ubuntu-karmic.dd.gz none 02:00 "~/experimental.sh"
+
+    ssh pipol pipol-sub esn i386-linux-ubuntu-lucid.dd.gz none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn amd64-linux-ubuntu-lucid.dd.gz none 02:00 "~/experimental.sh"
+
+    ssh pipol pipol-sub esn i386_kvm-linux-debian-testing none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn amd64_kvm-linux-debian-testing none 02:00 "~/experimental.sh"
+
+    ssh pipol pipol-sub esn i386-linux-fedora-core12.dd.gz none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn i386-linux-fedora-core13.dd.gz none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn i386_2010-linux-fedora-core14.dd.gz none 02:00 "~/experimental.sh"
+
+    ssh pipol pipol-sub esn amd64-linux-fedora-core12.dd.gz none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn amd64-linux-fedora-core13.dd.gz none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn amd64_2010-linux-fedora-core14.dd.gz none 02:00 "~/experimental.sh"
+
+    ssh pipol pipol-sub esn amd64-linux-opensuse-11.dd.gz none 02:00 "~/experimental.sh"
+
+    # ssh pipol pipol-sub esn i386-unix-solaris-10 none 02:00 "~/experimental.sh"
+
+    ssh pipol pipol-sub esn i386-linux-centos-5 none 02:00 "~/experimental.sh"
+    ssh pipol pipol-sub esn amd64-linux-centos-5 none 02:00 "~/experimental.sh"
+fi
--- pipol/upload_nightly.sh
+++ pipol/upload_nightly.sh
@@ -2,4 +2,14 @@
 
 # script to update the openmeeg nightly build config on pipol
 
-scp openmeeg*_nightly.sh pipol:~/.pipol/nightly/
+file=`mktemp`
+cp openmeeg_windows.sh $file
+cat >> $file <<EOF
+build_and_test "Nightly"
+EOF
+
+chmod +x $file
+scp $file pipol:~/.pipol/nightly/openmeeg_windows_nightly.sh
+rm $file
+
+scp openmeeg_nightly.sh pipol:~/.pipol/nightly/
