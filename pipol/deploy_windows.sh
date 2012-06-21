6c9c8ded8c451cc7e0a3f3c889ecd951eb6ca649
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2012-06-21 15:55:07 +0200

    Increase windows build time to 2:00 (like in linux).

--- pipol/deploy_windows.sh
+++ pipol/deploy_windows.sh
@@ -21,7 +21,7 @@ scp $file pipol:experimental_windows.sh
 rm $file
 
 # win xp 32bits
-ssh pipol pipol-sub esn i386_kvm-windows-xp-pro-sp3 none 01:00 "bash ~/experimental_windows.sh"
+ssh pipol pipol-sub esn i386_kvm-windows-xp-pro-sp3 none 02:00 "bash ~/experimental_windows.sh"
 
 # win 7 64bits
-ssh pipol pipol-sub esn amd64_kvm-windows-7 none 01:00 "bash ~/experimental_windows.sh"
+ssh pipol pipol-sub esn amd64_kvm-windows-7 none 02:00 "bash ~/experimental_windows.sh"
