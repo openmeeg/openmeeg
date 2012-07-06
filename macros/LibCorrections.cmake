bbad38ca49c8b00d954c0ca81b3dd4163e56c756
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2012-07-06 14:31:57 +0200

    Correctly install libraries on 64 bits archs. Simplify rpm packaging.

---
+++ macros/LibCorrections.cmake
@@ -0,0 +1,10 @@
+get_property(LIB64 GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS)
+
+if (${LIB64} STREQUAL "TRUE")
+    set(LIBSUFFIX 64)
+else()
+    set(LIBSUFFIX "")
+endif()
+
+set(INSTALL_LIB_DIR     lib${LIBSUFFIX}/Odyssee++ CACHE PATH "Installation directory for libraries")
+mark_as_advanced(INSTALL_LIB_DIR)
