eac1ff64e8500154bafa043b3d898e78767ddb21
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2012-07-20 21:32:44 +0200

    Use new variable for runtime output output.

--- macros/Win32Compat.cmake
+++ macros/Win32Compat.cmake
@@ -3,6 +3,10 @@
 # ==============================
 
 IF (WIN32)
+    SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${OpenMEEG_BINARY_DIR}/src")
+
+    #   These are obsolete as per cmake 2.6
+
     SET(LIBRARY_OUTPUT_PATH "${OpenMEEG_BINARY_DIR}/src")
     SET(EXECUTABLE_OUTPUT_PATH "${OpenMEEG_BINARY_DIR}/src")
 ENDIF()
