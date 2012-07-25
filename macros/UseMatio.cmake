5d506fd8782509e5f1fc4bdf46d583256d8330fb
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2012-07-25 13:14:48 +0200

    Simplify.

--- macros/UseMatio.cmake
+++ macros/UseMatio.cmake
@@ -6,23 +6,12 @@ OPTION(USE_SYSTEM_MATIO "Build the project using an already installed MATIO" OFF
 
 IF (USE_SYSTEM_MATIO)
     FIND_LIBRARY(MATIO_LIBRARIES matio)
-ENDIF()
-
-IF (NOT MATIO_LIBRARIES)
-    INCLUDE_DIRECTORIES(${OpenMEEG_SOURCE_DIR}/contrib/matio/src)
-    INCLUDE_DIRECTORIES(${OpenMEEG_SOURCE_DIR}/contrib/matio/zlib)
-    INCLUDE_DIRECTORIES(${OpenMEEG_BINARY_DIR}/contrib/matio/src)
-    INCLUDE_DIRECTORIES(${OpenMEEG_BINARY_DIR}/contrib/matio/zlib)
-
-    IF(WIN32)
-        INCLUDE_DIRECTORIES(${OpenMEEG_SOURCE_DIR}/contrib/matio/contrib/Windows)
+    IF (NOT MATIO_LIBRARIES)
+        MESSAGE(WARNING,"System's matio library not found. Using local version instead.")
     ENDIF()
-
-    SET(MATIO_LIBRARIES matio)
 ENDIF()
 
 IF (NOT MATIO_LIBRARIES)
-    MESSAGE(ERROR "There is a problem detecting Matio")
-ELSE()
+    SET(MATIO_LIBRARIES matio)
     SET(USE_MATIO 1)
 ENDIF()
