8780bf7e5f9041023703556b8caf5e2c0a5e3096
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2012-07-25 19:12:53 +0200

    Extend build time for windows 7 which is very slow...

--- pipol/deploy_windows.sh
+++ pipol/deploy_windows.sh
@@ -11,4 +11,4 @@ scp openmeeg_windows.sh pipol:openmeeg_windows.sh
 ssh pipol pipol-sub i386_kvm-windows-xp-pro-sp3 03:00 "bash ~/openmeeg_windows.sh $BRANCH"
 
 # win 7 64bits
-ssh pipol pipol-sub amd64_kvm-windows-7 03:00 "bash ~/openmeeg_windows.sh $BRANCH"
+ssh pipol pipol-sub amd64_kvm-windows-7 05:00 "bash ~/openmeeg_windows.sh $BRANCH"
