3e708b0c4606147506d9abffb24c0a86520529f7
Emmanuel Olivi <emmanuel.olivi@gmail.com>
2013-12-18 17:47:54 +0100

    Adding DGER for outerproduct.

--- libs/MatLib/MatLib/MatLibConfig.h
+++ libs/MatLib/MatLib/MatLibConfig.h
@@ -123,6 +123,7 @@ knowledge of the CeCILL-B license and that you accept its terms.
         double BLAS(ddot,DDOT)(const int&,const double*,const int&,const double*,const int&);
         double BLAS(dnrm2,DNRM2)(const int&,const double*,const int&);
         void BLAS(dscal,DSCAL)(const int&,const double&,double*,const int&);
+        void BLAS(dger,DGER)(const int&,const int&,const double&,const double*,const int&,const double*,const int&,double*,const int&);
         void BLAS(dspmv,DSPMV)(const char&,const int&,const double&,const double*,const double*,const int&,const double&,double*,const int&);
         void BLAS(dtpmv,DTPMV)(const char&,const char&,const char&,const int&,const double*,double*,const int&);
         void BLAS(dsymm,DSYMM)(const char&,const char&,const int&,const int&,const double&,const double*,const int&,const double*,const int&, const double&,double*,const int&);
@@ -143,7 +144,7 @@ knowledge of the CeCILL-B license and that you accept its terms.
     extern "C" {
         void FC_GLOBAL(dgesdd,DGESDD)(const char&,const int&,const int&,double*,const int&,double*,double*,const int&,double*,const int&,double*,const int&,int*,int&);
         void FC_GLOBAL(dpotf2,DPOTF2)(const char&,const int&,double*,const int&,int&);
-        double FC_GLOBAL(dlange,DLANGE)(const char&,const int&,const int&,double*,const int&,double*);
+        double FC_GLOBAL(dlange,DLANGE)(const char&,const int&,const int&,const double*,const int&,double*);
         void FC_GLOBAL(dsptrf,DSPTRF)(const char&,const int&,double*,int*,int&);
         void FC_GLOBAL(dtptri,DTPTRI)(const char&,const char&,const int&,double*,int&,int&);
         void FC_GLOBAL(dsptri,DSPTRI)(const char&,const int&,double*,int*,double*,int&);
