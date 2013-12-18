648865b1639655c87be7e6e48639bfd99452bb55
Emmanuel Olivi <emmanuel.olivi@gmail.com>
2013-12-18 17:49:56 +0100

    Adding small function.

--- libs/MatLib/MatLib/sparse_matrix.h
+++ libs/MatLib/MatLib/sparse_matrix.h
@@ -98,7 +98,9 @@ namespace OpenMEEG {
 
         const Tank& tank() const {return m_tank;}
 
+        void set( double t);
         Vector getlin(size_t i) const;
+        void setlin(Vector v, size_t i);
 
         void save(const char *filename) const;
         void load(const char *filename);
@@ -129,6 +131,13 @@ namespace OpenMEEG {
             else v(j)=0.0;
         }
         return v;
+    }
+
+    inline void SparseMatrix::setlin(Vector v, size_t i) {
+        assert(i<nlin());
+        for (size_t j=0;j<v.nlin();j++){
+            (*this)(i,j) = v(j);
+        }
     }
 }
 #endif  //! OPENMEEG_SPARSE_MATRIX_H
