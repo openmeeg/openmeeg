74a27cb3295105d6262c1695ba6dbd2a2e041f30
Emmanuel Olivi <emmanuel.olivi@gmail.com>
2013-12-19 17:32:42 +0100

    Correct for clapack.

--- libs/MatLib/MatLib/MatLibConfig.h
+++ libs/MatLib/MatLib/MatLibConfig.h
@@ -123,7 +123,7 @@ knowledge of the CeCILL-B license and that you accept its terms.
         double BLAS(ddot,DDOT)(const int&,const double*,const int&,const double*,const int&);
         double BLAS(dnrm2,DNRM2)(const int&,const double*,const int&);
         void BLAS(dscal,DSCAL)(const int&,const double&,double*,const int&);
-        void BLAS(dger,DGER)(const char&,const int&,const int&,const double&,const double*,const int&,const double*,const int&,double*,const int&);
+        void BLAS(dger,DGER)(const int&,const int&,const double&,const double*,const int&,const double*,const int&,double*,const int&);
         void BLAS(dspmv,DSPMV)(const char&,const int&,const double&,const double*,const double*,const int&,const double&,double*,const int&);
         void BLAS(dtpmv,DTPMV)(const char&,const char&,const char&,const int&,const double*,double*,const int&);
         void BLAS(dsymm,DSYMM)(const char&,const char&,const int&,const int&,const double&,const double*,const int&,const double*,const int&, const double&,double*,const int&);
