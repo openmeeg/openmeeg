59040e91b3d9db3d4ff0f94dfe8f55c176c52a44
Emmanuel Olivi <emmanuel.olivi@gmail.com>
2014-02-25 12:53:46 +0100

    updating examples and scripts

--- examples/compute_leadfields.py
+++ examples/compute_leadfields.py
@@ -10,60 +10,76 @@
 print __doc__
 
 import openmeeg as om
+from os import path as op
 
 ###############################################################################
 # Load data
-geom_file                    = 'head_model.geom'
-cond_file                    = 'head_model.cond'
-dipoles_file                 = 'cortex_dipoles.txt'
-squids_file                  = 'meg_channels_locations.squids'
-eeg_electrodes_file          = 'eeg_channels_locations.txt'
-ecog_electrodes_file         = 'ecog_electrodes_locations.txt'
-internal_electrodes_file     = 'internal_electrodes_locations.txt'
+geom_file                    = 'model/head_model.geom'
+cond_file                    = 'model/head_model.cond'
+dipoles_file                 = 'model/cortex_dipoles.txt'
+squids_file                  = 'model/meg_channels_locations.squids'
+eeg_electrodes_file          = 'model/eeg_channels_locations.txt'
+eit_electrodes_file          = 'model/eit_locations.txt'
+ecog_electrodes_file         = 'model/ecog_electrodes_locations.txt'
+internal_electrodes_file     = 'model/internal_electrodes_locations.txt'
 
-geom = om.Geometry()
-geom.read(geom_file, cond_file)
+geom = om.Geometry(geom_file, cond_file)
 
-dipoles = om.Matrix()
-dipoles.load(dipoles_file)
+dipoles = om.Matrix(dipoles_file)
 
-meg_sensors = om.Sensors()
-meg_sensors.load(squids_file)
+meg_sensors = om.Sensors(squids_file)
 
-eeg_electrodes = om.Sensors()
-eeg_electrodes.load(eeg_electrodes_file)
+eeg_electrodes = om.Sensors(eeg_electrodes_file)
 
-ecog_electrodes = om.Sensors()
-ecog_electrodes.load(ecog_electrodes_file)
+eit_electrodes = om.Sensors(eit_electrodes_file, geom.outermost_interface())
 
-int_electrodes = om.Matrix()
-int_electrodes.load(internal_electrodes_file)
+ecog_electrodes = om.Sensors(ecog_electrodes_file)
+
+int_electrodes = om.Matrix(internal_electrodes_file)
 
 ###############################################################################
+#create a dir for leadfields and tmp
+if not op.exists("tmp"):
+    import os
+    os.mkdir('tmp')
+if not op.exists("leadfields"):
+    import os
+    os.mkdir('leadfields')
+
 # Compute Leadfields
 gauss_order = 3
 use_adaptive_integration = True
 dipole_in_cortex = True
 
-hm = om.HeadMat(geom, gauss_order)
-hm.invert()
-hminv = hm
-# hminv = hm.inverse() # to also test the adjoint method: comment the 2 previous lines, and uncomment this line, and the two others containing 'adjoint'
-dsm = om.DipSourceMat(geom, dipoles, gauss_order,
-                                 use_adaptive_integration, dipole_in_cortex)
+if op.exists("tmp/hmi.mat"):
+    hminv = om.SymMatrix("tmp/hmi.mat")
+    print "HM inverse loaded from ", "tmp/hmi.mat"
+else:
+    hm = om.HeadMat(geom, gauss_order)
+    hm.invert()
+    hm.save("tmp/hmi.mat")
+    hminv = hm
+    # hminv = hm.inverse() # to also test the adjoint method: comment the 2 previous lines, and uncomment this line, and the two others containing 'adjoint'
+
+if op.exists("tmp/dsm.mat"):
+    dsm = om.Matrix("tmp/dsm.mat")
+    print "DSM loaded from ", "tmp/dsm.mat"
+else:
+    dsm = om.DipSourceMat(geom, dipoles, gauss_order, use_adaptive_integration, "Brain")
+    dsm.save("tmp/dsm.mat")
 
 # For EEG
 h2em = om.Head2EEGMat(geom, eeg_electrodes)
 
 # For ECoG
-h2ecogm = om.Head2ECoGMat(geom, ecog_electrodes)
+h2ecogm = om.Head2ECoGMat(geom, ecog_electrodes, "Cortex")
 
 # For MEG
 ds2mm = om.DipSource2MEGMat(dipoles, meg_sensors)
 h2mm = om.Head2MEGMat(geom, meg_sensors)
 
-# For EIT (using the same electrodes as EEG)
-eitsm = om.EITSourceMat(geom, eeg_electrodes, gauss_order)
+# For EIT
+eitsm = om.EITSourceMat(geom, eit_electrodes, gauss_order)
 
 # For Internal Potential
 iphm = om.Surf2VolMat(geom, int_electrodes)
@@ -75,10 +91,8 @@ meg_leadfield  = om.GainMEG(hminv, dsm, h2mm, ds2mm)
 eit_leadfield  = om.GainEEG(hminv, eitsm, h2em)
 ip_leadfield   = om.GainInternalPot(hminv, dsm, iphm, ipsm)
 sip_leadfield  = om.GainStimInternalPot(hminv, eitsm,  iphm)
-
 # eeg_leadfield_adjoint = om.GainEEGadjoint(geom,dipoles,hm, h2em)
 
-print "hm             : %d x %d" % (hm.nlin(), hm.ncol())
 print "hminv          : %d x %d" % (hminv.nlin(), hminv.ncol())
 print "dsm            : %d x %d" % (dsm.nlin(), dsm.ncol())
 print "h2em           : %d x %d" % (h2em.nlin(), h2em.ncol())
@@ -92,10 +106,10 @@ print "eit_leadfield  : %d x %d" % (eit_leadfield.nlin(), eit_leadfield.ncol())
 print "ip_leadfield   : %d x %d" % (ip_leadfield.nlin(), ip_leadfield.ncol())
 print "sip_leadfield  : %d x %d" % (sip_leadfield.nlin(), sip_leadfield.ncol())
 
-eeg_leadfield.save('eeg_leadfield.mat')
-# eeg_leadfield_adjoint.save('eeg_leadfield_adjoint.mat')
-ecog_leadfield.save('ecog_leadfield.mat')
-meg_leadfield.save('meg_leadfield.mat')
-eit_leadfield.save('eit_leadfield.mat')
-ip_leadfield.save('ip_leadfield.mat')
-sip_leadfield.save('sip_leadfield.mat')
+eeg_leadfield.save('leadfields/eeg_leadfield.mat')
+# eeg_leadfield_adjoint.save('leadfields/eeg_leadfield_adjoint.mat')
+ecog_leadfield.save('leadfields/ecog_leadfield.mat')
+meg_leadfield.save('leadfields/meg_leadfield.mat')
+eit_leadfield.save('leadfields/eit_leadfield.mat')
+ip_leadfield.save('leadfields/ip_leadfield.mat')
+sip_leadfield.save('leadfields/sip_leadfield.mat')
--- examples/compute_leadfields.sh
+++ examples/compute_leadfields.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-GEOMETRY=head_model.geom
-CONDUCTIVITIES=head_model.cond
-DIPOLES=cortex_dipoles.txt
-EEG_ELECTRODES=eeg_channels_locations.txt
-ECOG_ELECTRODES=ecog_electrodes_locations.txt
-SQUIDS=meg_channels_locations.squids
-INTERNAL_ELECTRODES=internal_electrodes_locations.txt
+GEOMETRY=model/head_model.geom
+CONDUCTIVITIES=model/head_model.cond
+DIPOLES=model/cortex_dipoles.txt
+EEG_ELECTRODES=model/eeg_channels_locations.txt
+EIT_ELECTRODES=model/eit_locations.txt
+ECOG_ELECTRODES=model/ecog_electrodes_locations.txt
+SQUIDS=model/meg_channels_locations.squids
+INTERNAL_ELECTRODES=model/internal_electrodes_locations.txt
 
 # Leadfields
-EEG_LEADFIELD=eeg_leadfield.mat
-EEG_LEADFIELD_ADJOINT=eeg_leadfield_adjoint.mat
-ECOG_LEADFIELD=ecog_leadfield.mat
-MEG_LEADFIELD=meg_leadfield.mat
-MEG_LEADFIELD_ADJOINT=meg_leadfield_adjoint.mat
-EIT_LEADFIELD=eit_leadfield.mat
-IP_LEADFIELD=ip_leadfield.mat
-SIP_LEADFIELD=sip_leadfield.mat
+EEG_LEADFIELD=leadfields/eeg_leadfield.mat
+EEG_LEADFIELD_ADJOINT=leadfields/eeg_leadfield_adjoint.mat
+ECOG_LEADFIELD=leadfields/ecog_leadfield.mat
+MEG_LEADFIELD=leadfields/meg_leadfield.mat
+MEG_LEADFIELD_ADJOINT=leadfields/meg_leadfield_adjoint.mat
+EIT_LEADFIELD=leadfields/eit_leadfield.mat
+IP_LEADFIELD=leadfields/ip_leadfield.mat
+SIP_LEADFIELD=leadfields/sip_leadfield.mat
 
 # Name temporary matrices
 HM=tmp/tmp.hm           # For EEG and MEG
@@ -31,6 +32,7 @@ IPHM=tmp/tmp.iphm       # For IP
 IPSM=tmp/tmp.ipsm       # For IP
 
 mkdir -p tmp
+mkdir -p leadfields
 
 # Compute EEG gain matrix
 om_assemble -HM ${GEOMETRY} ${CONDUCTIVITIES} ${HM}
@@ -39,10 +41,10 @@ om_assemble -DSM ${GEOMETRY} ${CONDUCTIVITIES} ${DIPOLES} ${DSM}
 om_assemble -H2EM ${GEOMETRY} ${CONDUCTIVITIES} ${EEG_ELECTRODES} ${H2EM}
 om_gain -EEG ${HMINV} ${DSM} ${H2EM} ${EEG_LEADFIELD}
 # or with the adjoint
-om_gain -EEGadjoint ${GEOMETRY} ${CONDUCTIVITIES} ${DIPOLES} ${HM} ${H2EM} ${EEG_LEADFIELD_ADJOINT}
+#om_gain -EEGadjoint ${GEOMETRY} ${CONDUCTIVITIES} ${DIPOLES} ${HM} ${H2EM} ${EEG_LEADFIELD_ADJOINT}
 
 # Compute ECoG gain matrix
-om_assemble -H2ECOGM ${GEOMETRY} ${CONDUCTIVITIES} ${ECOG_ELECTRODES} ${H2ECOGM}
+om_assemble -H2ECOGM ${GEOMETRY} ${CONDUCTIVITIES} ${ECOG_ELECTRODES} "Cortex" ${H2ECOGM}
 om_gain -EEG ${HMINV} ${DSM} ${H2ECOGM} ${ECOG_LEADFIELD}
 
 # Compute MEG gain matrix
@@ -53,7 +55,7 @@ om_gain -MEG ${HMINV} ${DSM} ${H2MM} ${DS2MEG} ${MEG_LEADFIELD}
 om_gain -MEGadjoint ${GEOMETRY} ${CONDUCTIVITIES} ${DIPOLES} ${HM} ${H2MM} ${DS2MEG} ${MEG_LEADFIELD_ADJOINT}
 
 # Compute EIT gain matrix
-om_assemble -EITSM ${GEOMETRY} ${CONDUCTIVITIES} ${EEG_ELECTRODES} ${EITSM}
+om_assemble -EITSM ${GEOMETRY} ${CONDUCTIVITIES} ${EIT_ELECTRODES} ${EITSM}
 om_gain -EEG ${HMINV} ${EITSM} ${H2EM} ${EIT_LEADFIELD}
 
 # Compute Internal Potential ....
