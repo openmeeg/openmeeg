dff5d7d1ffa545b34e2fd943ca84f734ddb032f1
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-03-12 20:55:21 +0100

    Remove svn requirement.

--- cmake/environment/CheckEnvironment.cmake
+++ cmake/environment/CheckEnvironment.cmake
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# medInria
+# OpenMEEG
 #
 # Copyright (c) INRIA 2013. All rights reserved.
 # See LICENSE.txt for details.
@@ -17,94 +17,56 @@
 # Assume Unix by default
 set(HOME_PATH $ENV{HOME})
 
-
-## #############################################################################
-## Look for windows specific programs
-## #############################################################################
+# Look for windows specific programs
 
 if (WIN32)
-  # Check for Visual Studio 2008 or 2010
-  if (NOT MSVC9 AND NOT MSVC10)
-    message(SEND_ERROR 
-      "MedInria can only be compiled with Visual Studio 2008 or 2010 at this time." 
-      )
-  endif()
+    # Check for Visual Studio 2008 or 2010
+    if (NOT MSVC9 AND NOT MSVC10)
+        message(SEND_ERROR "MedInria can only be compiled with Visual Studio 2008 or 2010 at this time.")
+    endif()
 
-  # Check for DirectX SDK (for VTK)
-  file(GLOB DIRECTX_SDK "C:/Program Files*/Microsoft DirectX SDK*")
-  if (NOT DIRECTX_SDK)
-    message(SEND_ERROR 
-      "You need to install Microsoft DirectX SDK." 
-      )
-  else()
-      mark_as_advanced(DIRECTX_SDK)
-  endif()
+    # Check for DirectX SDK (for VTK)
+    file(GLOB DIRECTX_SDK "C:/Program Files*/Microsoft DirectX SDK*")
+    if (NOT DIRECTX_SDK)
+        message(SEND_ERROR "You need to install Microsoft DirectX SDK.")
+    else()
+        mark_as_advanced(DIRECTX_SDK)
+    endif()
 
-  # GitBash
-  find_program(BASH_BIN NAMES bash)
-  if (NOT BASH_BIN)
-    message( SEND_ERROR 
-      "You need to install GitBash and add it to the PATH environment variable." 
-      )
-  else()
-      mark_as_advanced(BASH_BIN)
-  endif()
+    # GitBash
+    find_program(BASH_BIN NAMES bash)
+    if (NOT BASH_BIN)
+        message( SEND_ERROR "You need to install GitBash and add it to the PATH environment variable.")
+    else()
+        mark_as_advanced(BASH_BIN)
+    endif()
 
-  set(HOME_PATH $ENV{HOMEDRIVE}$ENV{HOMEPATH})
+    set(HOME_PATH $ENV{HOMEDRIVE}$ENV{HOMEPATH})
 endif()
 
+# Look for Git
 
-## #############################################################################
-## Look for Git
-## #############################################################################
 find_program(GIT_BIN NAMES git)
 if (NOT GIT_BIN)
-  message(SEND_ERROR 
-    "You need to install Git and add it to the PATH environment variable." 
-    )
+    message(SEND_ERROR "You need to install Git and add it to the PATH environment variable.")
 else()
-  mark_as_advanced(GIT_BIN)
+    mark_as_advanced(GIT_BIN)
 endif()
 
-## #############################################################################
-## Look for SSH
-## #############################################################################
+# Look for SSH
 
 find_program(SSH_BIN NAMES ssh)
 if (NOT SSH_BIN)
-  message(SEND_ERROR 
-    "You need to install SSH and add it to the PATH environment variable."
-    )
+    message(SEND_ERROR "You need to install SSH and add it to the PATH environment variable.")
 else()
-  mark_as_advanced(SSH_BIN)
+    mark_as_advanced(SSH_BIN)
 endif()
 
+# Test ssh access for github
 
-
-## #############################################################################
-## Look for Subversion
-## #############################################################################
-
-find_program(SVN_BIN NAMES svn)
-if (NOT SVN_BIN)
-  message(SEND_ERROR 
-    "You need to install Subversion and add it to the PATH environment variable." 
-    )
-else()
-  mark_as_advanced(SVN_BIN)
-endif()
-
-
-## #############################################################################
-## Test ssh access for github
-## #############################################################################
-
-set (SKIP_GITHUB_TESTS OFF CACHE BOOL 
-  "Set this to ON to skip GitHub access tests"
-  )
+set(SKIP_GITHUB_TESTS OFF CACHE BOOL "Set this to ON to skip GitHub access tests")
 mark_as_advanced(SKIP_GITHUB_TESTS)
 
 if (NOT ${SKIP_GITHUB_TESTS} AND ${USE_GITHUB_SSH})
-  include(GithubSshTest)
+    include(GithubSshTest)
 endif()
-
