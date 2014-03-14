7bae6c6e7610d559ec00baef04f5fca754cd19e7
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-03-14 16:16:44 +0100

    Update Windows script.

--- ci/JenkinsBuildScript-windows.bat
+++ ci/JenkinsBuildScript-windows.bat
@@ -1,6 +1,13 @@
+cd build
+
+cmake -DUSE_ATLAS=ON -DUSE_MKL=OFF -DENABLE_PYTHON=ON -DBUILD_TESTING=ON -DENABLE_PACKAGING=ON -DMATLAB_TESTING=OFF ..
+cmake --build . --config RelWithDebInfo
+cmake --build . --config RelWithDebInfo --target update
+cmake --build . --config RelWithDebInfo --target build
+
 del build\JUnitTestResults.xml
 pushd build
-ctest -T Test -C RelWithDebInfo --output-on-failure
+ctest.exe -T Test -C RelWithDebInfo --output-on-failure
 verify >nul
 popd
 "C:\Python27\python.exe" ci/CTest2JUnit.py build ci/CTest2JUnit.xsl > build/JUnitTestResults.xml
