990d01cceb82f6cccb881a9d61b35955de96e968
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-04-10 20:55:52 +0200

    Various changes merged from old SuperProject.

--- macros/UseMatio.cmake
+++ macros/UseMatio.cmake
@@ -2,26 +2,11 @@
 # MATIO library
 #------------------------------------------------------------
 
-OPTION(USE_SYSTEM_MATIO "Build the project using an already installed MATIO" OFF)
+option(USE_SYSTEM_MATIO "Build the project using an already installed MATIO" OFF)
 
-IF (USE_SYSTEM_MATIO)
-    FIND_LIBRARY(MATIO_LIBRARIES matio)
-    IF (NOT MATIO_LIBRARIES)
-        MESSAGE(WARNING,"System's matio library not found. Using local version instead.")
-    ENDIF()
-ENDIF()
-
-IF (NOT MATIO_LIBRARIES)
-
-    add_library(matio SHARED IMPORTED)
-    set_property(TARGET matio PROPERTY IMPORTED_LOCATION ${MATIO_DIR}/lib/libmatio.so)
-    SET(MATIO_LIBRARIES matio)
-    SET(USE_MATIO 1)
-
-    #   Not sure to understand why those should be here (this should be made by the matio
-    #   subdir). But they are.
-
-    INCLUDE_DIRECTORIES(${MATIO_DIR}/include)
-    INCLUDE_DIRECTORIES(${OpenMEEG_BINARY_DIR}/contrib/matio/zlib)
-
-ENDIF()
+if (USE_SYSTEM_MATIO)
+    find_library(MATIO_LIBRARIES matio)
+    if (NOT MATIO_LIBRARIES)
+        message(WARNING,"System's matio library not found. Using local version instead.")
+    endif()
+endif()
