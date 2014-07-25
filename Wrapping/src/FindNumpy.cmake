985ccc1d3cb0c04eb6574ff0b60e4da1828a5647
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-07-25 18:06:54 +0200

    Style + properly search for python interpreter.

--- Wrapping/src/FindNumpy.cmake
+++ Wrapping/src/FindNumpy.cmake
@@ -5,35 +5,34 @@
 #  PYTHON_NUMPY_FOUND, If false, do not try to use numpy headers.
 
 if (PYTHON_NUMPY_INCLUDE_DIR)
-  # in cache already
-  set (PYTHON_NUMPY_FIND_QUIETLY TRUE)
-endif (PYTHON_NUMPY_INCLUDE_DIR)
+    # in cache already
+    set (PYTHON_NUMPY_FIND_QUIETLY TRUE)
+endif()
 
-INCLUDE(FindPythonInterp)
+find_package(PythonInterp)
 
-IF(PYTHON_EXECUTABLE)
-    EXEC_PROGRAM ("${PYTHON_EXECUTABLE}"
-      ARGS "-c \"import numpy; print numpy.get_include()\""
-      #ARGS "-c 'import numpy; print numpy.get_include()'"
-      OUTPUT_VARIABLE PYTHON_NUMPY_INCLUDE_DIR
-      RETURN_VALUE PYTHON_NUMPY_NOT_FOUND)
+if (PYTHON_EXECUTABLE)
+    exec_program("${PYTHON_EXECUTABLE}"
+                 ARGS "-c \"import numpy; print numpy.get_include()\""
+                 OUTPUT_VARIABLE PYTHON_NUMPY_INCLUDE_DIR
+                 RETURN_VALUE PYTHON_NUMPY_NOT_FOUND)
 
     if (PYTHON_NUMPY_INCLUDE_DIR)
-      set (PYTHON_NUMPY_FOUND TRUE)
-      set (PYTHON_NUMPY_INCLUDE_DIR ${PYTHON_NUMPY_INCLUDE_DIR} CACHE STRING "Numpy include path")
+        set(PYTHON_NUMPY_FOUND TRUE)
+        set(PYTHON_NUMPY_INCLUDE_DIR ${PYTHON_NUMPY_INCLUDE_DIR} CACHE STRING "Numpy include path")
     else (PYTHON_NUMPY_INCLUDE_DIR)
-      set(PYTHON_NUMPY_FOUND FALSE)
-    endif (PYTHON_NUMPY_INCLUDE_DIR)
-ENDIF(PYTHON_EXECUTABLE)
+        set(PYTHON_NUMPY_FOUND FALSE)
+    endif()
+endif()
 
 if (PYTHON_NUMPY_FOUND)
-  if (NOT PYTHON_NUMPY_FIND_QUIETLY)
-    message (STATUS "Numpy headers found")
-  endif (NOT PYTHON_NUMPY_FIND_QUIETLY)
-else (PYTHON_NUMPY_FOUND)
-  if (PYTHON_NUMPY_FIND_REQUIRED)
-    message (FATAL_ERROR "Numpy headers missing")
-  endif (PYTHON_NUMPY_FIND_REQUIRED)
-endif (PYTHON_NUMPY_FOUND)
+    if (NOT PYTHON_NUMPY_FIND_QUIETLY)
+        message(STATUS "Numpy headers found")
+    endif()
+else()
+    if (PYTHON_NUMPY_FIND_REQUIRED)
+        message(FATAL_ERROR "Numpy headers missing")
+    endif()
+endif()
 
-MARK_AS_ADVANCED (PYTHON_NUMPY_INCLUDE_DIR)
+mark_as_advanced(PYTHON_NUMPY_INCLUDE_DIR)
