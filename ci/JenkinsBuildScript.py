b67148f867869b42b800921533b27a70f10f1421
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-08-07 17:02:44 +0200

    Add edbug messages.

--- ci/JenkinsBuildScript.py
+++ ci/JenkinsBuildScript.py
@@ -101,6 +101,8 @@ def cmake_configuration(args):
     if sys.platform=='win32':
         vsvers = set_visual_studio_environment()
         cmake_command_line.extend(['-G', 'Visual Studio '+unicode(vsvers), '-DCMAKE_BUILD_TYPE=RelWithDebInfo'])
+    print(os.environ)
+    CallAndLog('env','configure.log',args.debug)
 
     add_cmake_parameter(args.python,'ENABLE_PYTHON',cmake_command_line)
     add_cmake_parameter(args.documentation,'BUILD_DOCUMENTATION',cmake_command_line)
