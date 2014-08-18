b63e3c834feeca018afde076a3025804b769f774
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-08-18 11:53:02 +0200

    Style.

--- cmake/GiftiOption.cmake
+++ cmake/GiftiOption.cmake
@@ -2,26 +2,20 @@
 # GIFTI C library
 #------------------------------------------------------------
 
-OPTION(USE_GIFTI "Build the project using GIFTI IO support" OFF)
-MARK_AS_ADVANCED(USE_GIFTI)
+option(USE_GIFTI "Build the project using GIFTI IO support" OFF)
+mark_as_advanced(USE_GIFTI)
 
-IF (USE_GIFTI)
-    FIND_PACKAGE(EXPAT)
-    FIND_PACKAGE(ZLIB)
-    FIND_LIBRARY(NIFTI_LIBRARY niftiio)
-    FIND_LIBRARY(GIFTI_LIBRARY giftiio)
-    FIND_LIBRARY(ZNZ_LIBRARY znz)
-    SET(NIFTI_LIBRARIES ${EXPAT_LIBRARIES} ${ZLIB_LIBRARIES} ${NIFTI_LIBRARY} ${ZNZ_LIBRARY} m)
-    FIND_PATH(GIFTI_INCLUDE_PATH gifti_io.h PATHS /usr/include/gifti)
-    INCLUDE_DIRECTORIES(${GIFTI_INCLUDE_PATH})
-    FIND_PATH(NIFTI_INCLUDE_PATH nifti1_io.h PATHS /usr/include/nifti)
-    INCLUDE_DIRECTORIES(${NIFTI_INCLUDE_PATH})
-    SET(OPENMEEG_OTHER_INCLUDE_DIRECTORIES
-        ${OPENMEEG_OTHER_INCLUDE_DIRECTORIES}
-        ${GIFTI_INCLUDE_PATH}
-        ${NIFTI_INCLUDE_PATH})
-    SET(OPENMEEG_LIBRARIES 
-        ${OPENMEEG_LIBRARIES} 
-        ${GIFTI_LIBRARY} 
-        ${NIFTI_LIBRARIES})
-ENDIF()
+if (USE_GIFTI)
+    find_package(EXPAT)
+    find_package(ZLIB)
+    find_library(NIFTI_LIBRARY niftiio)
+    find_library(GIFTI_LIBRARY giftiio)
+    find_library(ZNZ_LIBRARY znz)
+    set(NIFTI_LIBRARIES ${EXPAT_LIBRARIES} ${ZLIB_LIBRARIES} ${NIFTI_LIBRARY} ${ZNZ_LIBRARY} m)
+    find_path(GIFTI_INCLUDE_PATH gifti_io.h PATHS /usr/include/gifti)
+    include_directories(${GIFTI_INCLUDE_PATH})
+    find_path(NIFTI_INCLUDE_PATH nifti1_io.h PATHS /usr/include/nifti)
+    include_directories(${NIFTI_INCLUDE_PATH})
+    set(OPENMEEG_OTHER_INCLUDE_DIRECTORIES ${OPENMEEG_OTHER_INCLUDE_DIRECTORIES} ${GIFTI_INCLUDE_PATH} ${NIFTI_INCLUDE_PATH})
+    set(OPENMEEG_LIBRARIES ${OPENMEEG_LIBRARIES} ${GIFTI_LIBRARY} ${NIFTI_LIBRARIES})
+endif()
