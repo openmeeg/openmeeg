9df0e1d022e116251ba0c9376e001563a9161915
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-08-24 19:10:29 +0200

    Attempt to generate needed PATH after build.

---
+++ cmake/TestConfigGeneration.cmake
@@ -0,0 +1,8 @@
+include(GetPrerequisites)
+
+get_prerequisites("${OM_ASSEMBLE_EXE}" PREREQUISITES 1 0 "${OM_ASSEMBLE_EXE}" "")
+foreach (i ${PREREQUISITES})
+    get_filename_component(dir ${i} REALPATH)
+    set(LIBRARY_PATHS "${dir};${LIBRARY_PATHS}")
+endforeach()
+file(WRITE ${CMAKE_BINARY_DIR}/TestConfig.cmake "set(ENV{PATH} \"${LIBRARY_PATHS}\$ENV{PATH}\")\n")
