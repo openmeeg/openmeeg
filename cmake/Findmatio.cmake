d7c25e79c7c162434a1242e085f23cb04eb8b54e
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2015-03-17 19:02:11 +0100

    Add a Findmatio.cmake file to properly find matio when not building it through the superproject. cmake/Findmatio.cmake

---
+++ cmake/Findmatio.cmake
@@ -0,0 +1,29 @@
+# Find the matio headers and library.
+#
+#  matio_INCLUDE_DIRS - where to find matio.h, etc.
+#  matio_LIBRARIES    - List of libraries.
+#  matio_FOUND        - True if matio found.
+
+find_package(HDF5 REQUIRED)
+
+# Look for the header file.
+find_path(matio_INCLUDE_DIR NAMES matio.h)
+mark_as_advanced(matio_INCLUDE_DIR)
+
+# Look for the library.
+find_library(matio_LIBRARY NAMES matio)
+mark_as_advanced(matio_LIBRARY)
+
+# handle the QUIETLY and REQUIRED arguments and set matio_FOUND to TRUE if 
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(matio DEFAULT_MSG matio_LIBRARY matio_INCLUDE_DIR)
+
+if (MATIO_FOUND)
+  set(matio_FOUND TRUE)
+  set(matio_LIBRARIES ${matio_LIBRARY} ${HDF5_LIBRARIES})
+  set(matio_INCLUDE_DIRS ${matio_INCLUDE_DIR} ${HDF5_INCLUDE_DIR})
+else()
+  set(matio_LIBRARIES)
+  set(matio_INCLUDE_DIRS)
+endif()
