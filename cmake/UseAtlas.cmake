e31b554345d2dd295696e8afdd46b7b0cecae4be
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2015-06-16 20:05:29 +0200

    Revisit Atlas discovery.

--- cmake/UseAtlas.cmake
+++ cmake/UseAtlas.cmake
@@ -43,47 +43,49 @@ if (USE_ATLAS)
         include_directories(/System/Library/Frameworks/vecLib.framework/Headers)
     else()
         set(ATLAS_LIB_SEARCHPATH
-            /usr/lib64/
-            /usr/lib64/atlas
-            /usr/lib64/atlas/sse2
-            /usr/lib/atlas/sse2
-            /usr/lib/sse2
-            /usr/lib64/atlas/sse3
-            /usr/lib/atlas/sse3
-            /usr/lib/sse3
-            /usr/lib/
-            /usr/lib/atlas
-            /usr/lib/atlas-base
-            /usr/lib64/atlas-base
-            )
-        set(ATLAS_LIBS atlas cblas f77blas clapack lapack blas)
+            /usr/lib64/atlas/sse3 /usr/lib/atlas/sse3 /usr/lib/sse3
+            /usr/lib64/atlas/sse2 /usr/lib/atlas/sse2 /usr/lib/sse2
+            /usr/lib64/atlas /usr/lib/atlas /usr/lib/atlas-base /usr/lib64/atlas-base
+            /usr/lib64/ /usr/lib/)
+
+        macro(find_atlas_lib)
+            set(lib_found FALSE)
+            foreach (LIB ${ARGN})
+                message("Searching: ${LIB}")
+                set(LIBNAMES ${LIB})
+                if (${LIB} STREQUAL "clapack")
+                    set(LIBNAMES ${LIB} lapack_atlas)
+                endif()
+                find_library(${LIB}_PATH
+                    NAMES ${LIBNAMES}
+                    PATHS ${ATLAS_LIB_SEARCHPATH}
+                    NO_DEFAULT_PATH
+                    NO_CMAKE_ENVIRONMENT_PATH
+                    NO_CMAKE_PATH
+                    NO_SYSTEM_ENVIRONMENT_PATH
+                    NO_CMAKE_SYSTEM_PATH)
+                if (${LIB}_PATH)
+                    get_filename_component(LAPACK_ROOT_DIR ${${LIB}_PATH} DIRECTORY)
+                    set(LAPACK_DLL_DIR ${LAPACK_ROOT_DIR}/bn)
+                    set(LAPACK_LIBRARIES ${LAPACK_LIBRARIES} ${${LIB}_PATH})
+                    mark_as_advanced(${LIB}_PATH)
+                    set(lib_found TRUE)
+                    break()
+                endif()
+            endforeach()
+            if (NOT lib_found)
+                message(WARNING "Could not find any of ${LIBS}")
+            endif()
+        endmacro()
 
         find_path(ATLAS_INCLUDE_PATH clapack.h /usr/include/atlas /usr/include/ NO_DEFAULT_PATH)
         find_path(ATLAS_INCLUDE_PATH clapack.h)
         mark_as_advanced(ATLAS_INCLUDE_PATH)
         include_directories(${ATLAS_INCLUDE_PATH})
-        foreach (LIB ${ATLAS_LIBS})
-            set(LIBNAMES ${LIB})
-            if (${LIB} STREQUAL "clapack")
-                set(LIBNAMES ${LIB} lapack_atlas)
-            endif()
-            find_library(${LIB}_PATH
-                NAMES ${LIBNAMES}
-                PATHS ${ATLAS_LIB_SEARCHPATH}
-                NO_DEFAULT_PATH
-                NO_CMAKE_ENVIRONMENT_PATH
-                NO_CMAKE_PATH
-                NO_SYSTEM_ENVIRONMENT_PATH
-                NO_CMAKE_SYSTEM_PATH)
-            if(${LIB}_PATH)
-                get_filename_component(LAPACK_ROOT_DIR ${${LIB}_PATH} DIRECTORY)
-                set(LAPACK_DLL_DIR ${LAPACK_ROOT_DIR}/bn)
-                set(LAPACK_LIBRARIES ${LAPACK_LIBRARIES} ${${LIB}_PATH})
-                mark_as_advanced(${LIB}_PATH)
-            else()
-                message(WARNING "Could not find ${LIB}")
-            endif()
-        endforeach()
+
+        find_atlas_lib(tatlas satlas atlas)
+        find_atlas_lib(clapack lapack)
+        find_atlas_lib(cblas f77blas blas)
     endif()
 endif()
 
