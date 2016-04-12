b40117575e26513ce6b396c9e7d91f2de7c7be0f
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-04-12 11:56:23 +0200

    update date

--- cmake/externals/projects_modules/clapack.cmake
+++ cmake/externals/projects_modules/clapack.cmake
@@ -1,6 +1,6 @@
 # OpenMEEG
 #
-# Copyright (c) INRIA 2013-2014. All rights reserved.
+# Copyright (c) INRIA 2013-2016. All rights reserved.
 # See LICENSE.txt for details.
 # 
 #  This software is distributed WITHOUT ANY WARRANTY; without even
