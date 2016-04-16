4c742b2d1cf25fcd09522d088e227200ba512d50
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-04-16 15:07:31 +0200

    cleanup refs to medinria in packaging

--- packaging/apple/PackagingConfiguration.cmake
+++ packaging/apple/PackagingConfiguration.cmake
@@ -17,20 +17,10 @@ set(CPACK_BINARY_PACKAGEMAKER OFF)
 #   Get distribution name and architecture
 
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CMAKE_SYSTEM_PROCESSOR}")
-set(CPACK_GENERATOR "PackageMaker;TGZ")
+# set(CPACK_GENERATOR "PackageMaker;TGZ")
+set(CPACK_GENERATOR "TGZ")
 
 set(PACKAGE_ARCH "MacOSX-Intel")
 if(BUILD_UNIVERSAL)
     set(PACKAGE_ARCH "Apple-Universal")
 endif()
-
-## #############################################################################
-## Add Apple packaging script
-## #############################################################################
-
-#configure_file(${CMAKE_SOURCE_DIR}/packaging/apple/ApplePackScript.cmake.in ${PROJECT_BINARY_DIR}/tmp.in)
-  
-#configure_file( ${PROJECT_BINARY_DIR}/tmp.in ${PROJECT_BINARY_DIR}/packaging/apple/ApplePackScript.cmake)
-#configure_file(${CMAKE_SOURCE_DIR}/packaging/apple/mac_packager.sh.in ${PROJECT_BINARY_DIR}/packaging/apple/mac_packager.sh)
-
-#set(CPACK_INSTALL_SCRIPT ${PROJECT_BINARY_DIR}/packaging/apple/ApplePackScript.cmake)
