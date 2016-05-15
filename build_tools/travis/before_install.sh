432837f4d31d1a04bb8f81a95e598bf7149a4c7a
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-05-15 10:37:20 +0200

    add travis config to use system libs

--- build_tools/travis/before_install.sh
+++ build_tools/travis/before_install.sh
@@ -22,4 +22,15 @@ else
 
     sudo apt-get update -qq
 
+    if [[ $USE_SYSTEM == "1" ]]; then
+        # to prevent IPv6 being used for APT
+        sudo bash -c "echo 'Acquire::ForceIPv4 \"true\";' > /etc/apt/apt.conf.d/99force-ipv4"
+        # The ultimate one-liner setup for NeuroDebian repository
+        bash <(wget -q -O- http://neuro.debian.net/_files/neurodebian-travis.sh)
+        # But we actually want -devel repository just to get matio backport
+        sudo sed -ie 's,neuro.debian.net/debian ,neuro.debian.net/debian-devel ,g' /etc/apt/sources.list.d/neurodebian.sources.list
+        sudo apt-get update
+        # Just to get information about available versions
+        apt-cache policy libmatio-dev
+    fi
 fi
--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -45,19 +45,35 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
       -DCMAKE_SKIP_RPATH:BOOL=OFF \
       ..
 else
-  cmake \
-      -DATLAS_INCLUDE_PATH:PATH=/usr/include/atlas \
-      -DBUILD_SHARED:BOOL=ON \
-      -DBUILD_DOCUMENTATION:BOOL=OFF \
-      -DBUILD_TESTING:BOOL=ON \
-      -DENABLE_PYTHON:BOOL=OFF \
-      -DENABLE_PACKAGING:BOOL=ON \
-      -DUSE_VTK:BOOL=OFF \
-      -DUSE_ATLAS:BOOL=OFF \
-      -DUSE_SYSTEM_MATIO:BOOL=OFF \
-      -DUSE_SYSTEM_hdf5:BOOL=OFF \
-      -DCMAKE_SKIP_RPATH:BOOL=OFF \
-      ..
+  if [[ $USE_SYSTEM == "1" ]]; then
+    cmake \
+        -DATLAS_INCLUDE_PATH:PATH=/usr/include/atlas \
+        -DBUILD_SHARED:BOOL=ON \
+        -DBUILD_DOCUMENTATION:BOOL=OFF \
+        -DBUILD_TESTING:BOOL=ON \
+        -DENABLE_PYTHON:BOOL=OFF \
+        -DENABLE_PACKAGING:BOOL=ON \
+        -DUSE_VTK:BOOL=OFF \
+        -DUSE_ATLAS:BOOL=ON \
+        -DUSE_SYSTEM_MATIO:BOOL=ON \
+        -DUSE_SYSTEM_hdf5:BOOL=ON \
+        -DCMAKE_SKIP_RPATH:BOOL=OFF \
+        ..
+  else
+    cmake \
+          -DATLAS_INCLUDE_PATH:PATH=/usr/include/atlas \
+          -DBUILD_SHARED:BOOL=ON \
+          -DBUILD_DOCUMENTATION:BOOL=OFF \
+          -DBUILD_TESTING:BOOL=ON \
+          -DENABLE_PYTHON:BOOL=OFF \
+          -DENABLE_PACKAGING:BOOL=ON \
+          -DUSE_VTK:BOOL=OFF \
+          -DUSE_ATLAS:BOOL=OFF \
+          -DUSE_SYSTEM_MATIO:BOOL=OFF \
+          -DUSE_SYSTEM_hdf5:BOOL=OFF \
+          -DCMAKE_SKIP_RPATH:BOOL=OFF \
+          ..
+  fi
 fi
 
 make
