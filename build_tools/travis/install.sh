e9d32b8dcd04f1ebe88d3fdb051dfe402a6d0b71
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-06-02 15:06:31 +0200

    missing hdf5 dep

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -14,7 +14,7 @@ else
 
     sudo apt-get update
     if [[ $USE_SYSTEM == "1" ]]; then
-      sudo apt-get install libatlas-dev libatlas-base-dev libblas-dev liblapack-dev
+      sudo apt-get install libatlas-dev libatlas-base-dev libblas-dev liblapack-dev libhdf5-dev
       # python-numpy swig python-dev libvtk5-dev libtiff4-dev doxygen
     fi
     # sudo apt-get install libblas-dev libatlas-base-dev liblapack-dev
