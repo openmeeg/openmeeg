bba8d40d220ed7193671198e6c9dcd322fcca2de
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-10-29 11:07:00 +0200

    add auto detection of veclib + update travis

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -61,7 +61,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   if [[ "$USE_PROJECT" == "0" ]]; then
     install_matio
 
-    # Build OpenMEEG
+    # Build OpenMEEG and use HDF5 from homebrew
     cmake \
     -DBUILD_SHARED:BOOL=ON \
     -DBUILD_DOCUMENTATION:BOOL=OFF \
@@ -75,7 +75,6 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   else
     if [[ "$USE_OPENBLAS" == "1" ]]; then
       cmake \
-      -DATLAS_INCLUDE_PATH:PATH=/usr/include/atlas \
       -DBUILD_DOCUMENTATION:BOOL=OFF \
       -DBUILD_TESTING:BOOL=ON \
       -DENABLE_PYTHON:BOOL=OFF \
@@ -87,9 +86,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
       -DUSE_SYSTEM_hdf5:BOOL=OFF \
       -DCMAKE_SKIP_RPATH:BOOL=OFF \
       ..
-    else
+    else  # use Atlas which maps to vecLib or Accelerate frameworks
       cmake \
-      -DATLAS_INCLUDE_PATH:PATH=/usr/include/atlas \
       -DBUILD_DOCUMENTATION:BOOL=OFF \
       -DBUILD_TESTING:BOOL=ON \
       -DENABLE_PYTHON:BOOL=OFF \
