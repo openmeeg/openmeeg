0ad2b12935374781b5c4a3a5e6375064ea3cec71
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-11-05 14:16:01 +0100

    test travis with python

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -1,40 +1,3 @@
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-
-    # Install some custom requirements on OS X
-    $CXX --version
-
-else
-    # # g++4.8.1
-    if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
-    if [ "$CXX" = "g++" ]; then export CXX="g++-4.8"; fi
-
-    # clang 3.4
-    if [ "$CXX" == "clang++" ]; then sudo apt-get install --allow-unauthenticated -qq clang-3.4; fi
-    if [ "$CXX" == "clang++" ]; then export CXX="clang++-3.4"; fi
-
-    sudo apt-get update
-    if [[ "$USE_SYSTEM" == "1" ]]; then
-      sudo apt-get install -y libhdf5-serial-dev libopenblas-base
-      if [[ "$USE_ATLAS" == "1" ]]; then
-          sudo apt-get install -y libatlas-dev libatlas-base-dev libblas-dev liblapack-dev
-      fi
-      if [[ "$USE_OPENBLAS" == "1" ]]; then
-          sudo apt-get install -y libopenblas-dev
-      fi
-      # libmatio-dev
-      # python-numpy swig python-dev libvtk5-dev libtiff4-dev doxygen
-    fi
-    # sudo apt-get install libblas-dev libatlas-base-dev liblapack-dev
-    # sudo apt-get install libvtk5-dev libtiff4-dev
-    # sudo apt-get install python-numpy swig python-dev
-    # sudo apt-get install doxygen
-    wget https://s3.amazonaws.com/biibinaries/thirdparty/cmake-3.0.2-Linux-64.tar.gz
-    tar -xzf cmake-3.0.2-Linux-64.tar.gz
-    sudo cp -fR cmake-3.0.2-Linux-64/* /usr
-    rm -rf cmake-3.0.2-Linux-64
-    rm cmake-3.0.2-Linux-64.tar.gz
-fi
-
 if [[ "$USE_PROJECT" == "0" ]]; then
   cd OpenMEEG
 fi
@@ -56,18 +19,22 @@ function install_matio {  # Install MATIO
   cd ../../
 }
 
+if [[ $USE_PYTHON == "1" ]]; then
+  ENABLE_PYTHON=ON
+else
+  ENABLE_PYTHON=OFF
+fi
+
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   if [[ "$USE_PROJECT" == "0" ]]; then
-    # install_matio
-
     if [[ "$USE_OPENBLAS" == "1" ]]; then
       # Build OpenMEEG and use openblas from homebrew
       cmake \
       -DBUILD_SHARED:BOOL=ON \
       -DBUILD_DOCUMENTATION:BOOL=OFF \
       -DBUILD_TESTING:BOOL=ON \
-      -DENABLE_PYTHON:BOOL=OFF \
+      -DENABLE_PYTHON:BOOL=${ENABLE_PYTHON} \
       -DENABLE_PACKAGING:BOOL=ON \
       -DUSE_VTK:BOOL=OFF \
       -DUSE_ATLAS:BOOL=OFF \
@@ -76,14 +43,13 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
       ..
     else  # use Atlas which maps to vecLib or Accelerate frameworks
       cmake \
+      -DBUILD_SHARED:BOOL=OFF \
       -DBUILD_DOCUMENTATION:BOOL=OFF \
       -DBUILD_TESTING:BOOL=ON \
-      -DENABLE_PYTHON:BOOL=OFF \
+      -DENABLE_PYTHON:BOOL=${ENABLE_PYTHON} \
       -DENABLE_PACKAGING:BOOL=ON \
       -DUSE_VTK:BOOL=OFF \
       -DUSE_ATLAS:BOOL=ON \
-      -DUSE_SYSTEM_matio:BOOL=OFF \
-      -DUSE_SYSTEM_hdf5:BOOL=OFF \
       -DCMAKE_SKIP_RPATH:BOOL=OFF \
       ..
     fi
@@ -92,7 +58,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
       cmake \
       -DBUILD_DOCUMENTATION:BOOL=OFF \
       -DBUILD_TESTING:BOOL=ON \
-      -DENABLE_PYTHON:BOOL=OFF \
+      -DENABLE_PYTHON:BOOL=${ENABLE_PYTHON} \
       -DENABLE_PACKAGING:BOOL=ON \
       -DUSE_VTK:BOOL=OFF \
       -DUSE_OPENBLAS:BOOL=ON \
@@ -105,7 +71,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
       cmake \
       -DBUILD_DOCUMENTATION:BOOL=OFF \
       -DBUILD_TESTING:BOOL=ON \
-      -DENABLE_PYTHON:BOOL=OFF \
+      -DENABLE_PYTHON:BOOL=${ENABLE_PYTHON} \
       -DENABLE_PACKAGING:BOOL=ON \
       -DUSE_VTK:BOOL=OFF \
       -DUSE_ATLAS:BOOL=ON \
@@ -126,7 +92,7 @@ else
         -DBUILD_SHARED:BOOL=ON \
         -DBUILD_DOCUMENTATION:BOOL=OFF \
         -DBUILD_TESTING:BOOL=ON \
-        -DENABLE_PYTHON:BOOL=OFF \
+        -DENABLE_PYTHON:BOOL=${ENABLE_PYTHON} \
         -DENABLE_PACKAGING:BOOL=ON \
         -DUSE_VTK:BOOL=OFF \
         -DUSE_ATLAS:BOOL=ON \
@@ -140,7 +106,7 @@ else
         -DBUILD_SHARED:BOOL=ON \
         -DBUILD_DOCUMENTATION:BOOL=OFF \
         -DBUILD_TESTING:BOOL=ON \
-        -DENABLE_PYTHON:BOOL=OFF \
+        -DENABLE_PYTHON:BOOL=${ENABLE_PYTHON} \
         -DENABLE_PACKAGING:BOOL=ON \
         -DUSE_VTK:BOOL=OFF \
         -DUSE_ATLAS:BOOL=OFF \
@@ -153,7 +119,7 @@ else
           -DATLAS_INCLUDE_PATH:PATH=/usr/include/atlas \
           -DBUILD_DOCUMENTATION:BOOL=OFF \
           -DBUILD_TESTING:BOOL=ON \
-          -DENABLE_PYTHON:BOOL=OFF \
+          -DENABLE_PYTHON:BOOL=${ENABLE_PYTHON} \
           -DENABLE_PACKAGING:BOOL=ON \
           -DUSE_VTK:BOOL=OFF \
           -DUSE_ATLAS:BOOL=OFF \
@@ -168,7 +134,7 @@ else
           -DATLAS_INCLUDE_PATH:PATH=/usr/include/atlas \
           -DBUILD_DOCUMENTATION:BOOL=OFF \
           -DBUILD_TESTING:BOOL=ON \
-          -DENABLE_PYTHON:BOOL=OFF \
+          -DENABLE_PYTHON:BOOL=${ENABLE_PYTHON} \
           -DENABLE_PACKAGING:BOOL=ON \
           -DUSE_VTK:BOOL=OFF \
           -DUSE_ATLAS:BOOL=OFF \
@@ -179,11 +145,6 @@ else
           ..
   fi
 fi
-
-# -DBUILD_SHARED:BOOL=ON \
-# -DBUILD_SHARED_LIBS_OpenMEEG:BOOL=ON
-# -DBUILD_SHARED_LIBS_hdf5:BOOL=ON -DBUILD_SHARED_LIBS_matio:BOOL=ON
-# -DBUILD_SHARED_LIBS_zlib:BOOL=ON
 
 make
 
