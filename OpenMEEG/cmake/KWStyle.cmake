17d295035220d8fc84e6f583c53e0413a49cc791
Emmanuel Olivi <Emmanuel.olivi@inria.fr>
2017-03-03 15:44:11 +0100

    Option KWStyle

--- OpenMEEG/cmake/KWStyle.cmake
+++ OpenMEEG/cmake/KWStyle.cmake
@@ -2,10 +2,7 @@
 # Code Syntax Checking with KWStyle
 #-----------------------------------------------
 
-option(CHECK_SYNTAX "Checking code syntax using KWStyle" OFF)
-mark_as_advanced(CHECK_SYNTAX)
-
-if (CHECK_SYNTAX)
+if (USE_KWSTYLE)
     add_custom_target(check_syntax
         COMMAND KWStyle -xml ${PROJECT_SOURCE_DIR}/OpenMEEGConfig.kws.xml -html ${PROJECT_BINARY_DIR}/KWStyleCheck -D KWStyleFilesToCheck.txt -v
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
