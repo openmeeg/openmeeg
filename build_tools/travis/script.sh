a27a16b27e50df92cdf3010a26c4e3c27982bbdc
eolivi <emmanuel.olivi@inria.fr>
2017-07-01 11:51:44 +0200

    travis: remove verbous tar output

--- build_tools/travis/script.sh
+++ build_tools/travis/script.sh
@@ -12,10 +12,13 @@ if [[ "$ENABLE_PACKAGING" == "1" ]]; then
 
     # now test the binary package: uninstall all brew, extract previously built package, run om_assemble
     if [[ $APPLE_STANDALONE == "1" ]]; then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-        tar xvvzf OpenMEEG-2.*.gz
+        # do not completly remove brew we need wget, ... for conda
+        # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+        brew uninstall --ignore-dependencies --force hdf5 libmatio vtk cgal openblas
+        tar xvvzf OpenMEEG-2.*.gz > /dev/null 2> /dev/null
         cd OpenMEEG-2.*
         export DYLD_LIBRARY_PATH="lib:$DYLD_LIBRARY_PATH"
         ./bin/om_assemble
+        cd ..
     fi
 fi
