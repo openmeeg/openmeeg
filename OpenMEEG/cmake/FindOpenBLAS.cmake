e270e2f0769f1fee444ab3d260e9566414cb7f41
eolivi <emmanuel.olivi@inria.fr>
2017-07-17 09:59:11 +0200

    find gfortran and quadmath on macOs

--- OpenMEEG/cmake/FindOpenBLAS.cmake
+++ OpenMEEG/cmake/FindOpenBLAS.cmake
@@ -29,8 +29,6 @@ else()
         /usr/local/lib
         /usr/local/lib64
         /usr/local/opt/openblas/lib
-        /usr/local/Cellar/
-        /usr/local/opt/gcc/lib
         /opt/OpenBLAS/lib
         $ENV{OpenBLAS}
         $ENV{OpenBLAS}/lib
@@ -76,7 +74,10 @@ endif()
 # if we are in a standalone mode look for other libs as well
 if ((${CMAKE_PROJECT_NAME} STREQUAL "OpenMEEG") AND STANDALONE)
     foreach(alib openblas lapacke quadmath gcc_s pthread gfortran gomp)
-        find_library(found${alib} NAMES ${alib} PATHS ${OpenBLAS_LIB_SEARCH_PATHS} PATH_SUFFIXES gcc/${CMAKE_CXX_COMPILER_VERSION} ${CMAKE_CXX_COMPILER_VERSION} gcc/x86_64-linux-gnu/${CMAKE_CXX_COMPILER_VERSION})
+        find_library(found${alib} NAMES ${alib} PATHS
+            ${OpenBLAS_LIB_SEARCH_PATHS}
+            /usr/local/Cellar/
+            PATH_SUFFIXES gcc/${CMAKE_CXX_COMPILER_VERSION} ${CMAKE_CXX_COMPILER_VERSION} gcc/x86_64-linux-gnu/${CMAKE_CXX_COMPILER_VERSION} gcc/4.9)
         if (found${alib})
             list(APPEND OpenMEEG_IMPORTED_LIBS ${found${alib}})
         endif()
