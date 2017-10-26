d4b40b0af79af5596e42acfdf1a5bfd5e6fe7961
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2017-10-26 16:29:41 +0200

    test

--- build_tools/travis/after_success.sh
+++ build_tools/travis/after_success.sh
@@ -5,13 +5,14 @@ if [[ "$USE_COVERAGE" == "1" ]]; then
     bash <(curl -s https://codecov.io/bash) > /dev/null 2>&1
 fi
 
+export PATH=${HOME}/miniconda_deploy/bin:$PATH
+conda update --yes --quiet conda
+conda create -n packageenv --yes pip python=2.7
+source activate packageenv
+conda install -y --quiet paramiko
+conda install -y --quiet pyopenssl
+
 # only upload to forge if we are on the master branch
 if [[ $ENABLE_PACKAGING == "1" && $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
-    export PATH=${HOME}/miniconda_deploy/bin:$PATH
-    conda update --yes --quiet conda
-    conda create -n packageenv --yes pip python=2.7
-    source activate packageenv
-    conda install -y --quiet paramiko
-    conda install -y --quiet pyopenssl
     python ${src_dir}/build_tools/upload_package_gforge.py *gz
 fi
