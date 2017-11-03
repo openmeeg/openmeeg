cb5ac46d3bfd3222b3941191c7657a0c9b76280d
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2017-11-03 14:03:16 +0100

    debug

--- build_tools/travis/after_success.sh
+++ build_tools/travis/after_success.sh
@@ -1,9 +1,9 @@
-if [[ "$USE_COVERAGE" == "1" ]]; then
-    lcov --directory . --capture --output-file coverage.info > /dev/null 2>&1 # capture coverage info
-    lcov --remove coverage.info '/usr/*' --output-file coverage.info > /dev/null 2>&1 # filter out system
-    lcov --list coverage.info > /dev/null 2>&1
-    bash <(curl -s https://codecov.io/bash) > /dev/null 2>&1
-fi
+# if [[ "$USE_COVERAGE" == "1" ]]; then
+#     lcov --directory . --capture --output-file coverage.info > /dev/null 2>&1 # capture coverage info
+#     lcov --remove coverage.info '/usr/*' --output-file coverage.info > /dev/null 2>&1 # filter out system
+#     lcov --list coverage.info > /dev/null 2>&1
+#     bash <(curl -s https://codecov.io/bash) > /dev/null 2>&1
+# fi
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     curl https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -o miniconda.sh -s
@@ -21,7 +21,7 @@ source activate packageenv
 conda install -y --quiet paramiko
 conda install -y --quiet pyopenssl
 
-# only upload to forge if we are on the master branch
-if [[ $ENABLE_PACKAGING == "1" && $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
-    python ${src_dir}/build_tools/upload_package_gforge.py *gz
-fi
+# # only upload to forge if we are on the master branch
+# if [[ $ENABLE_PACKAGING == "1" && $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
+#     python ${src_dir}/build_tools/upload_package_gforge.py *gz
+# fi
--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -1,46 +1,46 @@
-if [[ "$USE_PROJECT" == "0" ]]; then
-    cd OpenMEEG
-fi
+# if [[ "$USE_PROJECT" == "0" ]]; then
+#     cd OpenMEEG
+# fi
 
-mkdir build
-cd build
+# mkdir build
+# cd build
 
-if [[ "$USE_PROJECT" == "0" ]]; then
-    # Build OpenMEEG and use OpenBLAS from homebrew
-    # or use Atlas which maps to vecLib or Accelerate frameworks
-    cmake \
-        -DBUILD_DOCUMENTATION:BOOL=${BUILD_DOCUMENTATION} \
-        -DBUILD_TESTING:BOOL=ON \
-        -DENABLE_PYTHON:BOOL=${USE_PYTHON} \
-        -DENABLE_COVERAGE:BOOL=${USE_COVERAGE} \
-        -DENABLE_PACKAGING:BOOL=${ENABLE_PACKAGING} \
-        -DUSE_OMP:BOOL=${USE_OMP} \
-        -DUSE_VTK:BOOL=${USE_VTK} \
-        -DUSE_GIFTI:BOOL=${USE_GIFTI} \
-        -DUSE_CGAL:BOOL=${USE_CGAL} \
-        -DBLASLAPACK_IMPLEMENTATION:BOOL=${BLASLAPACK_IMPLEMENTATION} \
-        -DCMAKE_SKIP_RPATH:BOOL=OFF \
-        ..
-else
-    cmake \
-        -DBUILD_DOCUMENTATION:BOOL=${BUILD_DOCUMENTATION} \
-        -DBUILD_TESTING:BOOL=ON \
-        -DENABLE_PYTHON:BOOL=${USE_PYTHON} \
-        -DENABLE_COVERAGE:BOOL=${USE_COVERAGE} \
-        -DENABLE_PACKAGING:BOOL=${ENABLE_PACKAGING} \
-        -DUSE_OMP:BOOL=${USE_OMP} \
-        -DUSE_VTK:BOOL=${USE_VTK} \
-        -DUSE_GIFTI:BOOL=${USE_GIFTI} \
-        -DUSE_CGAL:BOOL=${USE_CGAL} \
-        -DAPPLE_STANDALONE:BOOL=${APPLE_STANDALONE} \
-        -DBLASLAPACK_IMPLEMENTATION:BOOL=${BLASLAPACK_IMPLEMENTATION} \
-        -DUSE_SYSTEM_clapack:BOOL=${USE_SYSTEM} \
-        -DUSE_SYSTEM_matio:BOOL=${USE_SYSTEM} \
-        -DUSE_SYSTEM_hdf5:BOOL=${USE_SYSTEM} \
-        -DUSE_SYSTEM_zlib:BOOL=${USE_SYSTEM} \
-        -DCMAKE_SKIP_RPATH:BOOL=OFF \
-        ..
-fi
+# if [[ "$USE_PROJECT" == "0" ]]; then
+#     # Build OpenMEEG and use OpenBLAS from homebrew
+#     # or use Atlas which maps to vecLib or Accelerate frameworks
+#     cmake \
+#         -DBUILD_DOCUMENTATION:BOOL=${BUILD_DOCUMENTATION} \
+#         -DBUILD_TESTING:BOOL=ON \
+#         -DENABLE_PYTHON:BOOL=${USE_PYTHON} \
+#         -DENABLE_COVERAGE:BOOL=${USE_COVERAGE} \
+#         -DENABLE_PACKAGING:BOOL=${ENABLE_PACKAGING} \
+#         -DUSE_OMP:BOOL=${USE_OMP} \
+#         -DUSE_VTK:BOOL=${USE_VTK} \
+#         -DUSE_GIFTI:BOOL=${USE_GIFTI} \
+#         -DUSE_CGAL:BOOL=${USE_CGAL} \
+#         -DBLASLAPACK_IMPLEMENTATION:BOOL=${BLASLAPACK_IMPLEMENTATION} \
+#         -DCMAKE_SKIP_RPATH:BOOL=OFF \
+#         ..
+# else
+#     cmake \
+#         -DBUILD_DOCUMENTATION:BOOL=${BUILD_DOCUMENTATION} \
+#         -DBUILD_TESTING:BOOL=ON \
+#         -DENABLE_PYTHON:BOOL=${USE_PYTHON} \
+#         -DENABLE_COVERAGE:BOOL=${USE_COVERAGE} \
+#         -DENABLE_PACKAGING:BOOL=${ENABLE_PACKAGING} \
+#         -DUSE_OMP:BOOL=${USE_OMP} \
+#         -DUSE_VTK:BOOL=${USE_VTK} \
+#         -DUSE_GIFTI:BOOL=${USE_GIFTI} \
+#         -DUSE_CGAL:BOOL=${USE_CGAL} \
+#         -DAPPLE_STANDALONE:BOOL=${APPLE_STANDALONE} \
+#         -DBLASLAPACK_IMPLEMENTATION:BOOL=${BLASLAPACK_IMPLEMENTATION} \
+#         -DUSE_SYSTEM_clapack:BOOL=${USE_SYSTEM} \
+#         -DUSE_SYSTEM_matio:BOOL=${USE_SYSTEM} \
+#         -DUSE_SYSTEM_hdf5:BOOL=${USE_SYSTEM} \
+#         -DUSE_SYSTEM_zlib:BOOL=${USE_SYSTEM} \
+#         -DCMAKE_SKIP_RPATH:BOOL=OFF \
+#         ..
+# fi
 
-cat CMakeCache.txt
-make
+# cat CMakeCache.txt
+# make
