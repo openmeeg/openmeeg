77d560335e9398166c453239e0a8fb7f7e523334
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2017-11-28 19:04:21 +0100

    Make sure MKL dll are found at test time.

--- OpenMEEG/cmake/CreateTestEnvironment.cmake
+++ OpenMEEG/cmake/CreateTestEnvironment.cmake
@@ -33,7 +33,7 @@ if (WIN32 OR APPLE)
     endif()
 
     set(DLL_DIRS "${ZLIB_ROOT}/${subdir}" "${HDF5_ROOT_DIR}/${subdir}" "${matio_ROOT_DIR}/${subdir}"
-                 "${CMAKE_MSVCIDE_RUN_PATH}" "${LAPACK_DLL_DIR}"
+                 "${CMAKE_MSVCIDE_RUN_PATH}" "${LAPACK_DLL_DIR}" ${MKL_LIBRARY_DIR}
                  "${PYTHON_OPENMEEG_MODULE_DIR}" "${VTK_LIBRARY_DIRS}" "${CGAL_LIBRARY_DIRS}" "${NIFTI_LIBRARY_DIR}")
     foreach (dir ${DLL_DIRS})
         FILE(TO_NATIVE_PATH "${dir}" dir)
