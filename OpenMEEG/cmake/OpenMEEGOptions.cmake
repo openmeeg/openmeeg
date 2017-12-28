faadf5476117a3aaaf8662a065ebd1438dd2940f
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2017-12-28 16:49:25 +0100

    fix rpath on mac to avoid setting DYLD_LIBRARY_PATH

--- OpenMEEG/cmake/OpenMEEGOptions.cmake
+++ OpenMEEG/cmake/OpenMEEGOptions.cmake
@@ -1,11 +1,13 @@
 # Tell CMake to use rpath with the libs we build
 
-set(CMAKE_MACOSX_RPATH 1)
 if (LINUX) # to fix the installed rpath so it looks in ../lib
     # https://www.semipol.de/2012/02/16/relative-rpath-settings-with-cmake.html
     SET(CMAKE_INSTALL_RPATH "$ORIGIN/../lib")
-else()
-    set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+elseif(APPLE)
+    SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
+    SET(CMAKE_MACOSX_RPATH 1)
+    SET(CMAKE_INSTALL_RPATH "@executable_path/../lib/")
+    SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 endif()
 
 # Set CMAKE_BUILD_TYPE to Release by default.
