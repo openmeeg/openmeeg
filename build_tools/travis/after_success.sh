1cf3716e73b28dc0d9ca99bef04ea8e39c282694
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2018-03-15 17:36:26 +0100

    put back build_tools

---
+++ build_tools/travis/after_success.sh
@@ -0,0 +1,28 @@
+if [[ "$USE_COVERAGE" == "1" ]]; then
+    lcov --directory . --capture --output-file coverage.info > /dev/null 2>&1 # capture coverage info
+    lcov --remove coverage.info '/usr/*' --output-file coverage.info > /dev/null 2>&1 # filter out system
+    lcov --list coverage.info > /dev/null 2>&1
+    bash <(curl -s https://codecov.io/bash) > /dev/null 2>&1
+fi
+
+
+# only upload to forge if we are on the master branch
+if [[ $ENABLE_PACKAGING == "1" && $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
+    if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+        curl https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -o miniconda.sh -s
+    else
+        wget -q http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
+    fi
+
+    chmod +x miniconda.sh
+    ./miniconda.sh -b -p ${HOME}/miniconda_deploy
+
+    export PATH=${HOME}/miniconda_deploy/bin:$PATH
+    conda update --yes --quiet conda
+    conda create -n packageenv --yes pip python=2.7
+    source activate packageenv
+    conda install -y --quiet paramiko
+    conda install -y --quiet pyopenssl
+
+    python ${src_dir}/build_tools/upload_package_gforge.py *gz
+fi
---
+++ build_tools/travis/script.sh
@@ -0,0 +1,56 @@
+# The -e flag causes the script to exit as soon as one command returns a non-zero exit code.
+set -e
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    find . -name "om_assemble" | xargs otool -L
+    find . -name "libOpenMEEG*dylib" | xargs otool -L
+else
+    find . -name "om_assemble" | xargs ldd
+    find . -name "libOpenMEEG*so" | xargs ldd
+fi
+
+if [[ "$USE_PROJECT" == "0" ]]; then
+    make CTEST_OUTPUT_ON_FAILURE=1 test;
+else
+    make CTEST_OUTPUT_ON_FAILURE=1 test-OpenMEEG;
+fi
+
+if [[ "$ENABLE_PACKAGING" == "1" ]]; then
+    cpack -G TGZ;
+
+    if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+        # remove completly brew to test standalone mac package
+        curl -O https://raw.githubusercontent.com/Homebrew/install/master/uninstall
+        chmod +x uninstall
+        ./uninstall --force
+    else
+        sudo apt-get remove -y libhdf5-serial-dev libmatio-dev libopenblas-dev liblapacke-dev libvtk5-dev libvtk5.8 libnifti-dev libgiftiio-dev libcgal-dev doxygen graphviz
+    fi
+
+    # Remove conda used from wrapping
+    echo "Removing miniconda_wrap"
+    rm -rf ${HOME}/miniconda_wrap
+
+    if [[ "$BLASLAPACK_IMPLEMENTATION" == "MKL" ]]; then
+        echo "Removing MKL"
+        sudo rm -rf /opt/intel
+    fi
+
+    echo "Untaring the package"
+    tar xzf OpenMEEG-2.*.gz #> /dev/null 2> /dev/null
+    cd OpenMEEG-2.*
+    if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+        echo "Running otool on om_assemble"
+        otool -L ./bin/om_assemble
+    else
+        echo "Running ldd on om_assemble"
+        ldd ./bin/om_assemble
+    fi
+    echo "running ./bin/om_assemble"
+    ./bin/om_assemble
+    echo "running ./bin/om_check_geom"
+    ./bin/om_check_geom -h
+    echo "running ./bin/om_matrix_info"
+    ./bin/om_matrix_info -h
+    cd ..
+fi
