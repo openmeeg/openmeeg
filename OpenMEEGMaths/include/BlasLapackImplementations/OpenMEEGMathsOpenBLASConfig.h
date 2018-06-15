19160a812e5cb06f17a77c328c06ca9427ac44cc
Joan Massich <sik@visor.udg.edu>
2018-06-15 09:40:42 +0200

    still works

--- OpenMEEGMaths/include/BlasLapackImplementations/OpenMEEGMathsOpenBLASConfig.h
+++ OpenMEEGMaths/include/BlasLapackImplementations/OpenMEEGMathsOpenBLASConfig.h
@@ -28,4 +28,4 @@
 
 #define DGESDD(X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,X11,X12,X13,X14) LAPACK(dgesdd,DGESDD)(LAPACK_COL_MAJOR,X1,X2,X3,X4,X5,X6,X7,X8,X9,X10)
 
-#include <BlasLapackImplementations/OpenMEEGMathsCBlasLapack.h>
+// #include <BlasLapackImplementations/OpenMEEGMathsCBlasLapack.h>
