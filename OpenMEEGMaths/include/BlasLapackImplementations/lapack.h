3fe38007181a1b5d9075fb9556c23bd97093f073
Joan Massich <sik@visor.udg.edu>
2018-06-18 19:27:46 +0200

    Remove all headers

--- OpenMEEGMaths/include/BlasLapackImplementations/lapack.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-extern "C" {
-    void LAPACK(dgetrf,DGETRF)(const int&,const int&,double*,const int&,int*,int&);
-    void LAPACK(dgetri,DGETRI)(const int&,double*,const int&,int*,double*,const int&,int&);
-}
