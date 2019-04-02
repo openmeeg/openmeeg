33b0afdd9ab78b354920dffbde842c3a0f57cff0
Joan Massich <mailsik@gmail.com>
2019-04-02 22:34:07 +0200

    Fix: gforge upload (#360)

--- build_tools/travis/after_success.sh
+++ build_tools/travis/after_success.sh
@@ -4,3 +4,19 @@ if [[ "$USE_COVERAGE" == "1" ]]; then
     lcov --list coverage.info > /dev/null 2>&1
     bash <(curl -s https://codecov.io/bash) > /dev/null 2>&1
 fi
+
+# only upload to forge if we are on the master branch
+if [[ $BUILD_PACKAGE == "ON" && $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
+    echo "Upload package"
+    if [ -x "$(command -v conda)" ]; then
+        git clone https://github.com/astropy/ci-helpers.git;
+        source ci-helpers/travis/setup_conda.sh;
+    fi
+
+    conda create -n packageenv --yes pip python=2.7
+    source activate packageenv
+    conda install -y --quiet paramiko
+    conda install -y --quiet pyopenssl
+
+    python ${src_dir}/build_tools/upload_package_gforge.py *gz
+fi
