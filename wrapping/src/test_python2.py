154e123840af1e99c1b32502e6cd6e31e4632ac8
Erwan Demairy <erwan.demairy@inria.fr>
2019-09-10 17:44:02 +0200

    cleaned useless print messages.

--- wrapping/src/test_python2.py
+++ wrapping/src/test_python2.py
@@ -4,10 +4,8 @@
 import sys
 import os
 topdir = os.getcwd()
-print(topdir)
+
 omdir  = os.path.join(topdir, "build/wrapping/src/")
-print(omdir)
-print(os.path.join(omdir , "_openmeeg.so"))
 if not os.path.exists( os.path.join(omdir , "_openmeeg.so")):
     print("Go to openmeeg topdir before launching this script")
     exit(-1)
