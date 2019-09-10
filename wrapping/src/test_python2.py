a1a92b9205cc7e26de7f470cb4b0ce4467ac19a4
Jean-Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2019-09-10 17:48:32 +0200

    prepared all tests

--- wrapping/src/test_python2.py
+++ wrapping/src/test_python2.py
@@ -3,20 +3,44 @@
 #
 import sys
 import os
+
+# import locally built openmeeg
 topdir = os.getcwd()
-print(topdir)
 omdir  = os.path.join(topdir, "build/wrapping/src/")
-print(omdir)
-print(os.path.join(omdir , "_openmeeg.so"))
 if not os.path.exists( os.path.join(omdir , "_openmeeg.so")):
     print("Go to openmeeg topdir before launching this script")
     exit(-1)
 
 sys.path.append(omdir)
-
 import openmeeg as om
 
+#
+fileskel = os.path.join(topdir, "data/Head1/Head1.")
 
+# dipole
 dipoles = om.Matrix()
-dipoles.load(os.path.join(topdir, "data/Head1/Head1.dip"))
+dipoles.load( fileskel + "dip")
 D = om.asarray(dipoles)
+print("D is a" , D.__class__)
+print(D)
+
+# TODO: sensors.read() using numpy arrays
+# TODO: sensors == [ double ]
+#sensors = om.Sensors()
+#sensors.load( fileskel + "squids")
+# TODO: D = asarray(sensors).copy...
+# TODO: sensors_1 = om.Matrix(D)
+
+# TODO: mesh.read() using numpy arrays
+# TODO: mesh == [ double ] , [ int ]
+#mesh = om.Mesh()
+#mesh.load( fileskel + "tri")
+# TODO: V = [...]
+# TODO: I = [...]
+# TODO: mesh_1 = om.Mesh(V, I)
+
+# TODO: geom.read() using raw python
+# TODO: conductivity == { string => double}
+# TODO: geometry     == { [ mesh ] , { string => [ int ] } }
+#geom = om.Geometry()
+#geom.read( fileskel + "geom" , fileskel + "cond" )
