44b359f3678f573582af3c618ba21be5260a2299
Jean-Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2019-09-10 16:22:15 +0200

    launch from depository topdir (works for every one)

--- wrapping/src/test_python2.py
+++ wrapping/src/test_python2.py
@@ -4,11 +4,13 @@
 import sys
 import os
 
-topdir = os.getcwd()
-if not topdir.endswith("/openmeeg"):
+omdir  = os.getcwd() + "/build/wrapping/src"
+
+if not os.path.exists( omdir + "_openmeeg.so"):
     print("Go to openmeeg topdir before launching this script")
     exit(-1)
 
+omdir  = topdir + "/build/wrapping/src"
 sys.path.append(topdir + "/build/wrapping/src")
 
 import openmeeg as om
@@ -17,3 +19,4 @@ import openmeeg as om
 dipoles = om.Matrix()
 dipoles.load(topdir+"/data/Head1/Head1.dip")
 D = om.asarray(dipoles)
+D
