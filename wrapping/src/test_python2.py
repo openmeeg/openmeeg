03ed1ddd7adc08a5c7681a741f96b6d76639cc46
Erwan Demairy <erwan.demairy@inria.fr>
2019-09-10 17:27:17 +0200

    Modifications to make the example work.

--- wrapping/src/test_python2.py
+++ wrapping/src/test_python2.py
@@ -3,20 +3,20 @@
 #
 import sys
 import os
-
-omdir  = os.getcwd() + "/build/wrapping/src"
-
-if not os.path.exists( omdir + "_openmeeg.so"):
+topdir = os.getcwd()
+print(topdir)
+omdir  = os.path.join(topdir, "build/wrapping/src/")
+print(omdir)
+print(os.path.join(omdir , "_openmeeg.so"))
+if not os.path.exists( os.path.join(omdir , "_openmeeg.so")):
     print("Go to openmeeg topdir before launching this script")
     exit(-1)
 
-omdir  = topdir + "/build/wrapping/src"
-sys.path.append(topdir + "/build/wrapping/src")
+sys.path.append(omdir)
 
 import openmeeg as om
 
 
 dipoles = om.Matrix()
-dipoles.load(topdir+"/data/Head1/Head1.dip")
+dipoles.load(os.path.join(topdir, "data/Head1/Head1.dip"))
 D = om.asarray(dipoles)
-D
