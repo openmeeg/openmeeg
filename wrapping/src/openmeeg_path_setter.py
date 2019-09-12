842b1271eb008043af2f6efe4d9c22b570a2f40d
Jean-Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2019-09-12 20:38:53 +0200

    better heuristic to find _openmeeg.so - do not need to be into wrapping/src

--- wrapping/src/openmeeg_path_setter.py
+++ wrapping/src/openmeeg_path_setter.py
@@ -6,9 +6,20 @@ import os
 
 # import locally built openmeeg
 topdir = os.getcwd()
-omdir  = os.path.join(topdir, "build/wrapping/src/")
-if not os.path.exists( os.path.join(omdir , "_openmeeg.so")):
-    print("Go to openmeeg topdir before launching this script")
-    exit(-1)
 
-sys.path.append(omdir)
+# heuristic
+found = 0;
+l = ""
+for i in range(1, 10):
+    d = os.path.join( topdir, l , "build" , "wrapping", "src")
+    if os.path.exists( os.path.join( d , "_openmeeg.so")):
+        print(d)
+        sys.path.append(d)
+        found = 1
+        break
+    l = os.path.join(l , "..")
+
+if found == 0:
+    print("Cannot find locally compiled _openmeeg.so file")
+    print("Please goto openmeeg repository or a subdir of it")
+    exit(-1)
