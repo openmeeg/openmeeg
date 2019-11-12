8b168c70b727fdc695029b3702875826469cd5df
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2019-11-12 16:07:21 +0100

    Minor renaming to make code clearer.

--- OpenMEEG/src/danielsson.cpp
+++ OpenMEEG/src/danielsson.cpp
@@ -120,8 +120,8 @@ namespace OpenMEEG
     double dist_point_interface(const Vect3& p,const Interface& interface,Vect3& alphas,Triangle& nearestTriangle) {
         double distmin = std::numeric_limits<double>::max();
 
-        for (const auto& interface : interface.oriented_meshes())
-            for (const auto& triangle : interface.mesh().triangles()) {
+        for (const auto& omesh : interface.oriented_meshes())
+            for (const auto& triangle : omesh.mesh().triangles()) {
                 bool  inside;
                 Vect3 alphasLoop;
                 const double distance = dist_point_triangle(p,triangle,alphasLoop,inside);
