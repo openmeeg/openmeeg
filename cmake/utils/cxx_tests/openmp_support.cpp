80ff1af5399a05e44fe098bbeec9b1c40238487e
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2019-12-24 03:19:30 +0100

    Debug attempt.

--- cmake/utils/cxx_tests/openmp_support.cpp
+++ cmake/utils/cxx_tests/openmp_support.cpp
@@ -7,14 +7,17 @@ main() {
     for (unsigned i=0;i<100;++i)    
         V.push_back(i); 
     #pragma omp parallel for
-    #if defined OPENMP_RANGEFOR
+    #if defined RANGEFOR
     for (const auto& val : V) {
-    #elif defined OPENMP_ITERATOR
+    #warning "RANGEFOR"
+    #elif defined ITERATOR
     for (std::vector<unsigned>::const_iterator vi=V.begin();vi!=V.end();++vi) {
         const unsigned val = *vi;
+    #warning "ITERATOR"
     #else
     for (unsigned i=0;i<V.size();++i) {
         const unsigned val = V[i];
+    #warning "UNSIGNED"
     #endif
         std::cerr << val << std::endl;
     }
