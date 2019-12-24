83110f8832538f26f4c9b9f1fd54e7d656b68672
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2019-12-24 13:39:39 +0100

    remove unused old cmake files

--- cmake_old/GithubOption.cmake
+++
@@ -1,2 +0,0 @@
-option(USE_GITHUB_SSH "Use by default Git SSH addresses, requires public key set on github" OFF)
-mark_as_advanced(USE_GITHUB_SSH)
--- cmake_old/README.txt
+++
@@ -1,4 +0,0 @@
-This is for legacy reasons.
-
-If some of the cmake features from previous release are missing you will
-find them here.
