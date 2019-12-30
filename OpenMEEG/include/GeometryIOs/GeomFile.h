e4c3ef064557198f4eedafdb0574a335de2b4316
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2019-12-30 02:40:45 +0100

    Introduce Geometry IOs.

---
+++ OpenMEEG/include/GeometryIOs/GeomFile.h
@@ -0,0 +1,348 @@
+﻿/*
+Project Name : OpenMEEG
+
+© INRIA and ENPC (contributors: Geoffray ADDE, Maureen CLERC, Alexandre
+GRAMFORT, Renaud KERIVEN, Jan KYBIC, Perrine LANDREAU, Théodore PAPADOPOULO,
+Emmanuel OLIVI
+Maureen.Clerc.AT.inria.fr, keriven.AT.certis.enpc.fr,
+kybic.AT.fel.cvut.cz, papadop.AT.inria.fr)
+
+The OpenMEEG software is a C++ package for solving the forward/inverse
+problems of electroencephalography and magnetoencephalography.
+
+This software is governed by the CeCILL-B license under French law and
+abiding by the rules of distribution of free software.  You can  use,
+modify and/ or redistribute the software under the terms of the CeCILL-B
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info".
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's authors,  the holders of the
+economic rights,  and the successive licensors  have only  limited
+liability.
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL-B license and that you accept its terms.
+*/
+
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+
+#include <interface.h>
+#include <GeometryExceptions.H>
+#include <IOUtils.H>
+#include <GeometryIO.h>
+#include <mesh_ios.h>
+
+namespace OpenMEEG::GeometryIOs {
+
+    // Read the head file description (.geom files) and load the information into a Geometry.
+
+    // Check file data/README.rst for complete details about the .geom format.
+    // The syntax of the head description is a header ("# Domain Description 1.1") followed
+    // by two or three sections:
+    //
+    //     - The first section is optional (for backward compatibility).
+    //       Starting with the keyword "MeshFile", it follows the path to the VTK/vtp file containing the meshes.
+    //       OR
+    //       Starting with the keyword "Meshes", it follows the number of meshes, and the paths to the meshes
+    //       (with the keyword "Mesh").
+    //
+    //     - The second section is introduced by the keyword "Interfaces" followed by a number
+    //       (the number of interfaces) and optionally the keyword "Mesh" (for backward compatibility).
+    //       The section contains the list of interfaces preceded by keyword "Interface".
+    //
+    //     - The third section is introduced by the keyword "Domains" and the number of domains
+    //       (everything else on the line containing the keyword is currently ignored). The section
+    //       contains domains descriptions, one per line. Each domain consist of:
+    //
+    //         o a domain name.
+    //         o a list of IDs (signed numbers or signed names): the sign ('+'by default) of the ID depicts 
+    //           whether the interior or the exterior of the interface should be used to select the domain.
+    //
+    // Any line starting with # is considered a comment and is silently ignored.
+
+    /// \brief Reader for geometry file (.geom).
+
+    class OPENMEEG_EXPORT GeomFile: public GeometryIO {
+
+        typedef GeometryIO base;
+
+        void load_meshes(Geometry& geometry) override;
+
+        typedef enum { UNKNOWN_VERSION=-1, VERSION10, VERSION11 } VersionId;
+
+        const char* name() const override { return "geom"; }
+        Matrix load_data() const { return Matrix(); }
+
+        virtual void save_geom(const Geometry& geometry) const { }
+        virtual void save_data(const Matrix& matrix)     const { }
+        virtual void write() const { }
+
+        GeometryIO* clone(const std::string& filename) const override { return new GeomFile(filename); }
+
+        void load_domains(Geometry& geometry) override;
+
+        VersionId version(const unsigned major,const unsigned minor) const {
+            VersionId id = UNKNOWN_VERSION;
+            if (major==1) {
+                if (minor==0)
+                    id = VERSION10;
+                if (minor==1)
+                    id = VERSION11;
+            }
+            return id;
+        }
+
+        VersionId version() const { return version_id; }
+
+        static std::string default_name(const unsigned n) {
+            std::stringstream res;
+            res << n;
+            return res.str();
+        }
+
+        std::string given_name(const std::string& keyword) {
+            std::string res;
+            ifs >> io_utils::match(keyword) >> io_utils::token(res,':');
+            return res;
+        }
+
+        std::string filename() {
+            std::string filename;
+            ifs >> io_utils::filename(filename,'"',false);
+            return (is_relative_path(filename)) ? directory+filename : filename;
+        }
+
+        static bool extract_sign(std::string& str,const bool mandatory=false) {
+            const char sign = str[0];
+            if (sign=='-' || sign=='+') {
+                str = str.substr(1,str.size());
+                return sign=='+';
+            }
+            if (mandatory)
+                throw "";
+            return true;
+        }
+
+        std::string section_name(const unsigned n,const std::string& keyword) {
+            bool unnamed;
+            ifs >> io_utils::skip_comments("#") >> io_utils::match_optional(keyword+':',unnamed);
+            const std::string& name = (unnamed || version_id==VERSION10) ? default_name(n+1) : given_name(keyword);
+            return name;
+        }
+
+        void read_mesh_descriptions(const unsigned n,const std::string& keyword) {
+            for (unsigned i=0; i<n; ++i) {
+                const std::string& name = section_name(i,keyword);
+                const std::string& path = filename();
+                mesh_descriptions.push_back({ name, MeshIO::create(path) });
+            }
+        }
+
+        //  Extract the tokens on the remainder of the line in the stream.
+
+        std::vector<std::string> get_line_tokens() {
+            std::string line;
+            std::getline(ifs,line);
+            std::istringstream iss(line);
+            typedef std::istream_iterator<std::string> Iterator;
+            std::vector<std::string> res;
+            std::copy(Iterator(iss),Iterator(),std::back_inserter(res));
+            return res;
+        }
+
+        void read_meshes(Geometry& geometry) {
+
+            geometry.meshes().reserve(mesh_descriptions.size());
+
+            // First read the total number of vertices
+
+            for (auto& desc : mesh_descriptions) {
+                desc.io->open();
+                desc.io->load_points(geometry); 
+            }
+
+            // Second really load the meshes
+
+            for (const auto& desc : mesh_descriptions) {
+                geometry.meshes().emplace_back(geometry,desc.name);
+                Mesh& mesh = geometry.meshes().back();
+                desc.io->load_triangles(mesh);
+                mesh.update(true);
+            }
+        }
+
+        struct MeshDescription {
+            std::string name;
+            MeshIO*     io;
+        };
+
+        typedef std::vector<MeshDescription> MeshDescriptions;
+
+        GeomFile(const std::string& filename=""): base(filename,"geom") { }
+
+        static const GeomFile prototype;
+
+        VersionId        version_id;
+        std::ifstream    ifs;
+        std::string      directory;
+        MeshDescriptions mesh_descriptions;
+        bool             mesh_provided_as_interfaces;
+        unsigned         nb_interfaces;
+    };
+
+    void GeomFile::load_meshes(Geometry& geometry) {
+        
+        ifs.open(fname.c_str());
+
+        if (!ifs.is_open())
+            throw OpenMEEG::OpenError(fname);
+
+        //  Get the version of the geometry file format.
+
+        unsigned major,minor; ///< version of the domain description
+        ifs >> io_utils::match("# Domain Description ") >> major >> io_utils::match(".") >> minor;
+
+        if (ifs.fail())
+            throw OpenMEEG::WrongFileFormat(fname);
+
+        version_id = version(major,minor);
+        if (version_id==VERSION10)
+            std::cerr << "(DEPRECATED) Please consider updating your geometry file to the new format 1.1 (see data/README.rst): "
+                      << fname << std::endl;
+
+        if (version_id==UNKNOWN_VERSION) {
+             std::cerr << "Domain Description version not available !" << std::endl;
+             throw OpenMEEG::WrongFileFormat(fname);
+        }
+
+        // Extract the absolute path of geometry file
+
+        directory = absolute_path(fname);
+
+        // Process meshes.
+
+        bool has_meshfile;
+        bool has_meshsection;
+
+        if (version_id==VERSION11) {
+
+            // Read the mesh section of the description file.
+            // Try to load the meshfile (VTK::vtp file) or try to load the meshes
+
+            ifs >> io_utils::skip_comments("#") >> io_utils::match_optional("MeshFile",has_meshfile);
+
+            if (has_meshfile) {
+                GeometryIO* io = GeometryIO::create(filename());
+                io->load(geometry);
+            }
+
+            ifs >> io_utils::skip_comments("#") >> io_utils::match_optional("Meshes",has_meshsection);
+            if (has_meshsection) {
+                unsigned nb_meshes;
+                ifs >> nb_meshes;
+                read_mesh_descriptions(nb_meshes,"Mesh");
+            }
+        }
+
+        // Process interfaces.
+
+        bool trash; // backward compatibility, catch "Mesh" optionally.
+        ifs >> io_utils::skip_comments('#')
+            >> io_utils::match("Interfaces") >> nb_interfaces >> io_utils::match_optional("Mesh", trash);
+
+        if (ifs.fail())
+            throw OpenMEEG::WrongFileFormat(fname);
+
+        mesh_provided_as_interfaces = !has_meshfile && !has_meshsection;
+        if (mesh_provided_as_interfaces)
+            read_mesh_descriptions(nb_interfaces,"Interface");
+
+        read_meshes(geometry);
+    }
+
+    void GeomFile::load_domains(Geometry& geometry) {
+
+        // Create interfaces
+
+        Interfaces interfaces;
+
+        if (mesh_provided_as_interfaces) {
+            for (unsigned i=0; i<mesh_descriptions.size(); ++i) {
+                Interface interface(mesh_descriptions.at(i).name);
+                interface.oriented_meshes().push_back(OrientedMesh(geometry.meshes().at(i)));
+                interfaces.push_back(interface);
+            }
+        } else {
+            for (unsigned i=0; i<nb_interfaces; ++i) {
+                const std::string& interfacename = section_name(i,"Interface");
+                interfaces.push_back(interfacename);
+                for (auto& token : get_line_tokens()) {
+                    const OrientedMesh::Orientation oriented = (extract_sign(token)) ? OrientedMesh::Normal : OrientedMesh::Opposite;
+                    interfaces[i].oriented_meshes().push_back(OrientedMesh(geometry.mesh(token),oriented));
+                }
+            }
+        }
+
+        std::map<std::string,Interface&> interfaces_map;
+        for (auto& interface : interfaces) {
+            interfaces_map.insert({ interface.name(), interface });
+            if (!interface.is_mesh_orientations_coherent()) { // check and correct global orientation
+                std::cerr << "Interface \"" << interface.name() << "\" is not closed !" << std::endl
+                          << "Please correct a mesh orientation when defining the interface in the geometry file." << std::endl;
+                exit(1);
+            }
+        }
+
+        //  Load domains.
+
+        unsigned num_domains;
+        ifs >> io_utils::skip_comments('#') >> io_utils::match("Domains") >> num_domains;
+
+        if (ifs.fail())
+            throw OpenMEEG::WrongFileFormat(fname);
+
+        geometry.domains().resize(num_domains);
+        for (auto& domain : geometry.domains()) {
+            ifs >> io_utils::skip_comments('#') >> io_utils::match("Domain");
+            if (version_id==VERSION10) { // backward compatibility
+                ifs >> domain.name();
+            } else {
+                ifs >> io_utils::token(domain.name(),':');
+            }
+            for (auto& token : get_line_tokens()) {
+                if (token=="shared") {
+                    std::cerr << "(DEPRECATED) Ignoring the useless shared keyword. Please consider updating the geometry file " << fname
+                              << " to the new 1.1 format (see data/README.rst)." << std::endl;
+                    break;                    
+                }
+                const SimpleDomain::Side side = (extract_sign(token)) ? SimpleDomain::Outside : SimpleDomain::Inside;
+                try {
+                    Interface& interface = interfaces_map.at(token);
+                    domain.boundaries().push_back(SimpleDomain(interface,side));
+                } catch(...) {
+                    throw OpenMEEG::NonExistingDomain<std::string>(domain.name(),token);
+                }
+            }
+        }
+
+        if (ifs.fail())
+            throw OpenMEEG::WrongFileFormat(fname);
+    }
+}
---
+++ OpenMEEG/include/GeometryIOs/vtp.h
@@ -0,0 +1,322 @@
+/*
+Project Name : OpenMEEG
+
+© INRIA and ENPC (contributors: Geoffray ADDE, Maureen CLERC, Alexandre
+GRAMFORT, Renaud KERIVEN, Jan KYBIC, Perrine LANDREAU, Théodore PAPADOPOULO,
+Emmanuel OLIVI
+Maureen.Clerc.AT.inria.fr, keriven.AT.certis.enpc.fr,
+kybic.AT.fel.cvut.cz, papadop.AT.inria.fr)
+
+The OpenMEEG software is a C++ package for solving the forward/inverse
+problems of electroencephalography and magnetoencephalography.
+
+This software is governed by the CeCILL-B license under French law and
+abiding by the rules of distribution of free software.  You can  use,
+modify and/ or redistribute the software under the terms of the CeCILL-B
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info".
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's authors,  the holders of the
+economic rights,  and the successive licensors  have only  limited
+liability.
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL-B license and that you accept its terms.
+*/
+
+#pragma once
+
+#include <map>
+#include <sstream>
+
+#ifdef USE_VTK
+#include <vtkPolyData.h>
+#include <vtkPoints.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkXMLPolyDataWriter.h>
+#include <vtkCellArray.h>
+#include <vtkDoubleArray.h>
+#include <vtkUnsignedIntArray.h>
+#include <vtkPointData.h>
+#include <vtkCellData.h>
+#include <vtkDataArray.h>
+#include <vtkAbstractArray.h>
+#include <vtkStringArray.h>
+#include <vtkSmartPointer.h>
+#endif
+
+namespace OpenMEEG::GeometryIOs {
+
+    /// \brief Handling of VTK\\vtp files.
+
+#ifdef USE_VTK
+
+    class OPENMEEG_EXPORT Vtp: public GeometryIO {
+
+        typedef GeometryIO base;
+
+    public:
+
+        void open(const std::string& filename) { vtpfilename = filename; }
+
+        /// \brief load a VTK\\vtp file \param filename into a mesh. Optionally read some associated data in matrix \param data if \param READ_DATA is true.
+
+        void load_mehes(Geometry& geometry) {
+            vtkSmartPointer<vtkXMLPolyDataReader> reader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
+            reader->SetFileName(vtpfilename.c_str());
+            reader->Update();
+
+            vtkMesh = reader->GetOutput();
+
+            const unsigned npts  = vtkMesh->GetNumberOfPoints();
+            const unsigned ntrgs = vtkMesh->GetNumberOfCells();
+
+            //  Check that the mesh is a triangulation.
+
+            for (unsigned i=0; i<ntrgs; ++i)
+                if (vtkMesh->GetCellType(i)!=VTK_TRIANGLE) {
+                    std::cerr << "This is not a triangulation" << std::endl;
+                    exit(1);
+                }
+            
+            //  Add vertices.
+
+            int trash;
+            vtkSmartPointer<vtkUnsignedIntArray> v_indices = vtkUnsignedIntArray::SafeDownCast(vtkMesh->GetPointData()->GetArray("Indices",trash));
+
+            geometry.vertices().reserve(npts);
+
+            for (unsigned i=0; i<npts; ++i) {
+                geometry.vertices().push_back(Vertex(vtkMesh->GetPoint(i)[0],vtkMesh->GetPoint(i)[1],vtkMesh->GetPoint(i)[2]));
+                if (trash!=-1) // index provided
+                    geometry.vertices().back().index() = v_indices->GetValue(i);
+            }
+
+            // Create meshes using the different meshes names associated with the cells.
+
+            vtkSmartPointer<vtkStringArray> cell_id = vtkStringArray::SafeDownCast(vtkMesh->GetCellData()->GetAbstractArray("Names"));
+
+            std::set<std::string> meshes_name;
+            std::map<const std::string,unsigned> mesh_map;
+            for (unsigned i=0; i<ntrgs; ++i)
+                if (meshes_name.insert(cell_id->GetValue(i)).second) {
+                    geometry.meshes().push_back(Mesh(*this,name));
+                    mesh_map.insert({ name, geometry.meshes().size()-1 }):
+                }
+
+            // Insert the triangle and mesh vertices addresses into the right mesh
+
+            vtkSmartPointer<vtkUnsignedIntArray> c_indices = vtkUnsignedIntArray::SafeDownCast(vtkMesh->GetCellData()->GetArray("Indices",trash));
+            for (unsigned i=0; i<ntrgs; ++i) {
+                const std::string& mesh_name = cell_id->GetValue(i);
+                Mesh& mesh = geometry.meshes().at(mesh_map.at(mesh_name));
+                Triangles& triangles = mesh.triangles();
+
+                const vtkSmartPointer<vtkIdList>& vl = vtkMesh->GetCell(i)->GetPointIds();
+                triangles.push_back(Triangle(geometry.vertices()[vl->GetId(0)],geometry.vertices()[vl->GetId(1)],geometry.vertices()[vl->GetId(2)]));
+                if (trash!=-1) // index provided
+                    triangles.back().index() = c_indices->GetValue(i);
+            }
+
+            for (auto& mesh : geometry.meshes()) {
+                mesh.build_mesh_vertices();
+                mesh.update(true);
+            }
+        }
+
+        Matrix load_data() const {
+
+            const unsigned nl = vtkMesh->GetNumberOfPoints()+vtkMesh->GetNumberOfCells(); 
+            const unsigned nc = vtkMesh->GetPointData()->GetNumberOfArrays()-1;
+
+            if (nl==0 || nc==0)
+                return;
+
+            Matrix data(nl,nc);
+            for (unsigned j=0; j<nc; ++j) {
+                std::ostringstream sic;
+                sic << j;
+                int trash;
+                const std::string& pot_array_name = "Potentials-"+sic.str();
+                const unsigned potsz = vtkMesh->GetPointData()->GetArray(pot_array_name.c_str(),trash)->GetSize();
+                for (unsigned i=0; i<potsz; ++i) {
+                    const unsigned ind = vtkUnsignedIntArray::SafeDownCast(vtkMesh->GetPointData()->GetArray("Indices",trash))->GetValue(i);
+                    data(ind,j) = vtkDoubleArray::SafeDownCast(vtkMesh->GetPointData()->GetArray(pot_array_name.c_str(),trash))->GetValue(i);
+                }
+                const std::string& curr_array_name = "Currents-"+sic.str();
+                const unsigned currsz = vtkMesh->GetCellData()->GetArray(curr_array_name.c_str(),trash)->GetSize();
+                for (unsigned i=0; i<currsz; ++i) {
+                    const unsigned ind = vtkUnsignedIntArray::SafeDownCast(vtkMesh->GetCellData()->GetArray("Indices",trash))->GetValue(i);
+                    data(ind,j) = vtkDoubleArray::SafeDownCast(vtkMesh->GetCellData()->GetArray(curr_array_name.c_str(),trash))->GetValue(i);
+                }
+            }
+            return data;
+        }
+
+        /// \brief write a VTK\\vtp file.
+
+        void save(const Geometry& geometry) const {
+
+            vtkMesh = vtkSmartPointer<vtkPolyData>::New();
+
+            //  Vertices
+
+            std::map<const Vertex*,unsigned> map;
+            vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+            vtkSmartPointer<vtkUnsignedIntArray> point_indices = vtkSmartPointer<vtkUnsignedIntArray>::New();
+            point_indices->SetName("Indices");
+
+            unsigned i = 0;
+            for (const auto& vertex : vertices()) {
+                points->InsertNextPoint(vertex(0),vertex(1),vertex(2));
+                point_indices->InsertNextValue(vertex.index());
+                map[&vertex] = i++;
+            }
+
+            vtkMesh->SetPoints(points);
+            vtkMesh->GetPointData()->AddArray(point_indices);
+
+            //  Normals.
+
+            vtkSmartPointer<vtkDoubleArray> normals = vtkSmartPointer<vtkDoubleArray>::New();
+            normal-->SetNumberOfComponents(3); // 3d normals (ie x, y, z)
+            normals->SetName("Normals");
+
+            // TODO: Not finished.
+
+            //  Triangles.
+
+            vtkSmartPointer<vtkCellArray> cells  = vtkSmartPointer<vtkCellArray>::New(); // the triangles
+
+            vtkSmartPointer<vtkStringArray> cell_id = vtkSmartPointer<vtkStringArray>::New(); // ids/mesh names
+            cell_id->SetName("Names");
+
+            vtkSmartPointer<vtkUnsignedIntArray> cell_indices = vtkSmartPointer<vtkUnsignedIntArray>::New(); // indices
+            cell_indices->SetName("Indices");
+
+            for(const auto& mesh : geometry.meshes())
+                for (const auto& triangle: mesh.triangles()) {
+                    const vtkIdType vtktriangle[3] = { map[&(triangle.vertex(0))], map[&(triangle.vertex(1))], map[&(triangle.vertex(2))] };
+                    cells->InsertNextCell(3,vtktriangle);
+                    cell_id->InsertNextValue(mesh.name());
+                    cell_indices->InsertNextValue(triangle.index());
+                }
+
+            vtkMesh->SetPolys(cells);
+            vtkMesh->GetCellData()->AddArray(cell_id);
+            vtkMesh->GetCellData()->AddArray(cell_indices);
+
+        }
+
+        void save_data(const Matrix& data) const {
+            // Check the data corresponds to the geometry
+
+            const bool HAS_OUTERMOST = (data.nlin()==geometry.meshes().size()); // data has least p values ?
+            if (!HAS_OUTERMOST)
+                om_error(data.nlin()==geometry.meshes().size()-outermost_interface().nb_triangles());
+
+            std::vector<vtkSmartPointer<vtkDoubleArray>> potentials(data.ncol()); // potential on vertices
+            std::vector<vtkSmartPointer<vtkDoubleArray>> currents(data.ncol()); // current on triangles
+
+            for (unsigned j = 0; j < data.ncol(); ++j) {
+                std::stringstream sdip;
+                sdip << j;
+
+                potentials[j] = vtkSmartPointer<vtkDoubleArray>::New();
+                potentials[j]->SetName(("Potentials-"+sdip.str()).c_str());
+
+                currents[j]   = vtkSmartPointer<vtkDoubleArray>::New();
+                currents[j]->SetName(("Currents-"+sdip.str()).c_str());
+
+                for (const auto& vertex : vertices())
+                    potentials[j]->InsertNextValue(data(vertex.index(),j));
+
+                for(const auto& mesh : geometry.meshes())
+                    for (const auto& triangle: mesh.triangles())
+                        currents[j]->InsertNextValue(((mesh.outermost() && !HAS_OUTERMOST) ? 0.0 : data(triangle.index(),j)));
+            }
+
+            for (unsigned j=0;j<data.ncol();++j) {
+                vtkMesh->GetPointData()->AddArray(potentials[j]);
+                vtkMesh->GetCellData()->AddArray(currents[j]);
+            }
+        }
+
+        void write() {
+            vtkSmartPointer<vtkXMLPolyDataWriter> writer = vtkSmartPointer<vtkXMLPolyDataWriter>::New();
+            writer->SetFileName(vtpfilename.c_str());
+
+            #if VTK_MAJOR_VERSION <= 5
+            writer->SetInput(vtkMesh);
+            #else
+            writer->SetInputData(vtkMesh);
+            #endif
+            writer->Write();
+        }
+
+    private:
+
+        const char* name() const override { return "vtp"; }
+        Matrix load_data() const { return Matrix(); }
+
+        virtual void save_geom(const Geometry& geometry) const { }
+        virtual void save_data(const Matrix& matrix)     const { }
+        virtual void write() const { }
+
+        GeometryIO* clone(const std::string& filename) const override { return new Vtp(filename); }
+
+        Vtp(const std::string& filename=""): base(filename,"vtp") { }
+
+        static const Vtp prototype;
+
+        std::string                  vtpfilename;
+        vtkSmartPointer<vtkPolyData> vtkMesh;
+    };
+#else
+
+    class OPENMEEG_EXPORT Vtp: public GeometryIO {
+
+        typedef GeometryIO base;
+
+    public:
+
+        const char* name() const override { return "vtp"; }
+
+    protected:
+
+        void load_meshes(Geometry&) override {
+            std::cerr << "Error: please specify USE_VTK to cmake" << std::endl; // TODO in Legacy format ? // Exceptions
+        }
+
+        Matrix load_data() const override {
+            std::cerr << "Error: please specify USE_VTK to cmake" << std::endl; // TODO in Legacy format ? // Exceptions
+            throw "";
+        }
+
+    private:
+
+        virtual void save_geom(const Geometry& geometry) const override { }
+        virtual void save_data(const Matrix& matrix)     const override { }
+        virtual void write() const override { }
+
+        GeometryIO* clone(const std::string& filename) const override { return new Vtp(filename); }
+
+        Vtp(const std::string& filename=""): base(filename,"vtp") { }
+
+        static const Vtp prototype;
+    };
+#endif
+}
