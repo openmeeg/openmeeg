d0b406e47ffda8d93f53915021a1434a539a6613
Eric Larson <larson.eric.d@gmail.com>
2022-06-20 01:19:35 +0100

    FIX: Try to fix warnings

--- OpenMEEGMaths/include/vector.h
+++ OpenMEEGMaths/include/vector.h
@@ -98,15 +98,15 @@ namespace OpenMEEG {
             return res;
         }
 
-        void operator+=(const Vector& v);
-        void operator-=(const Vector& v);
-        void operator*=(const double x);
+        inline void operator+=(const Vector& v);
+        inline void operator-=(const Vector& v);
+        inline void operator*=(const double x);
         void operator/=(const double x) { (*this) *= (1.0/x); }
-        Vector operator+(const double i) const;
-        Vector operator-(const double i) const;
-        Vector operator*(const double x) const;
+        inline Vector operator+(const double i) const;
+        inline Vector operator-(const double i) const;
+        inline Vector operator*(const double x) const;
         Vector operator/(const double x) const { return (*this)*(1.0/x); }
-        double operator*(const Vector& v) const;
+        inline double operator*(const Vector& v) const;
         Vector operator*(const Matrix& m) const;
 
         Vector kmult(const Vector& x) const;
