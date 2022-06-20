2e929929bceb4d18f0fd4771ed36df76cea204e7
Eric Larson <larson.eric.d@gmail.com>
2022-06-20 02:11:34 +0100

    FIX: op

--- OpenMEEGMaths/include/vector.h
+++ OpenMEEGMaths/include/vector.h
@@ -105,6 +105,9 @@ namespace OpenMEEG {
         inline Vector operator*(const double x) const;
         Vector operator/(const double x) const { return (*this)*(1.0/x); }
         inline double operator*(const Vector& v) const;
+        Vector operator+(const double i) const;
+        Vector operator-(const double i) const;
+        Vector operator*(const Matrix& m) const;
 
         Vector kmult(const Vector& x) const;
         // Vector conv(const Vector& v) const;
