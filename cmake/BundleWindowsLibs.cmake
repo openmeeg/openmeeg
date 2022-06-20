7e1ce23c5373432153f1cda40914e062d39f488f
Eric Larson <larson.eric.d@gmail.com>
2022-06-20 03:12:39 +0100

    FIX: Make part of swig step

--- cmake/BundleWindowsLibs.cmake
+++
@@ -1,8 +0,0 @@
-#---------------------------------------------------------------
-# Bundle windows libraries
-#---------------------------------------------------------------
-
-if (WIN32)
-    set (CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
-    set (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
-endif()
