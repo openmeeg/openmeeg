6eb29e8df61ba2e8458ae9cbb41ca96c95f5e783
Eric Larson <larson.eric.d@gmail.com>
2022-07-08 15:51:10 -0400

    FIX: Fix for modern MSVC

--- OpenMEEGMaths/include/BlasLapackImplementations/OpenMEEGMathsOpenBLASConfig.h
+++ OpenMEEGMaths/include/BlasLapackImplementations/OpenMEEGMathsOpenBLASConfig.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#if WIN32
+#if defined(_MSC_VER)
+    #include <complex.h>
     #define LAPACK_COMPLEX_CUSTOM
-    #define lapack_complex_float float
-    #define lapack_complex_double double
+    #define lapack_complex_float _Fcomplex
+    #define lapack_complex_double _Dcomplex
 #endif
 
 #include <cblas.h>
