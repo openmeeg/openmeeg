4bbb3c7e7b7ccdf5ca82541f4fbed7b63be1f3d0
Eric Larson <larson.eric.d@gmail.com>
2022-07-14 14:54:54 -0400

    FIX: Plat

--- build_tools/cibuildwheel_before_build.sh
+++ build_tools/cibuildwheel_before_build.sh
@@ -7,24 +7,25 @@ if [[ "$1" == "" ]]; then
     echo "Usage: $0 <PROJECT_PATH>"
     exit 1
 fi
-ROOT = $1
-echo "Using project root for ${CIBW_PLATFORM}: ${ROOT}"
+ROOT=$1
+PLATFORM=$(python -c "import sys; print(sys.platform)")
+echo "Using project root for platform \"${PLATFORM}\": \"${ROOT}\""
 pip install cmake
-if [[ "$CIBW_PLATFORM" == "linux" ]]; then
+if [[ "$PLATFORM" == "linux" ]]; then
     sudo apt -yq install liblapacke-dev libhdf5-dev libmatio-dev libopenblas-dev
-elif [[ "$CIBW_PLATFORM" == "osx" ]]; then
+elif [[ "$PLATFORM" == "darwin" ]]; then
     brew install hdf5 libmatio boost swig openblas
     BLAS_DIR=/usr/local/opt/openblas
     OPENBLAS_INCLUDE=$BLAS_DIR/include
     OPENBLAS_LIB=$BLAS_DIR/lib
     export CMAKE_CXX_FLAGS="-I$OPENBLAS_INCLUDE -L$OPENBLAS_LIB"
     export CMAKE_PREFIX_PATH="$BLAS_DIR"
-elif [[ "$CIBW_PLATFORM" == "win" ]]; then
+elif [[ "$PLATFORM" == "win32" ]]; then
     source ${ROOT}/build_tools/setup_windows_compilation.sh
     source ${ROOT}/build_tools/download_openblas_windows.sh
     pip install delvewheel
 else
-    echo "Unknown platform: ${CIBW_PLATFORM}"
+    echo "Unknown platform: ${PLATFORM}"
     exit 1
 fi
 export PYTHON_OPT=-DENABLE_PYTHON=OFF
