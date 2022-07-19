4beff62c7c9a6581769a5cb5a940bd1c76be48ad
Eric Larson <larson.eric.d@gmail.com>
2022-07-19 15:40:16 -0500

    FIX: More

--- build_tools/cibuildwheel_test_command.sh
+++ build_tools/cibuildwheel_test_command.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+if [[ "$1" != "" ]]; then
+    export OPENMEEG_DATA_PATH=$1/data
+fi
 set -xe
 pytest $(python -c 'from pathlib import Path; import openmeeg; print(Path(openmeeg.__file__).parent)')
