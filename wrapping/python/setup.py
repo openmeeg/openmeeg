e4f07585e02b522b0de8f614d26a642c7162093d
Eric Larson <larson.eric.d@gmail.com>
2022-07-19 10:44:04 -0400

    WIP: Enable cibuildwheel Windows

--- wrapping/python/setup.py
+++ wrapping/python/setup.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     ext_modules = []
     if os.getenv('OPENMEEG_USE_SWIG', '0').lower() in ('1', 'true'):
         include_dirs = [np.get_include()]
-        swig_opts = ['-c++', '-v', '-Werror']
+        swig_opts = ['-c++', '-v', '-O']  # TODO: , '-Werror']
         library_dirs = []
         openmeeg_include = os.getenv('OPENMEEG_INCLUDE')
         if openmeeg_include is not None:
