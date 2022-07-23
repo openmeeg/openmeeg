b11660ecb7302366b465938883fb8617d2dba0f3
Eric Larson <larson.eric.d@gmail.com>
2022-07-23 11:27:23 -0500

    FIX: Only set if not set

--- build_tools/cibw_test_command.sh
+++ build_tools/cibw_test_command.sh
@@ -5,7 +5,11 @@ if [[ "$1" != "" ]]; then
 fi
 # TODO: We need to reenable the tests that this comments out. Something about
 # how cmake SWIG wraps is better than how setuptools does it
-export OPENMEEG_BAD_TYPE=1
-export OPENMEEG_BAD_MSVC=$(python -c "import sys; print(int(sys.platform=='win32'))")
+if [[ "$OPENMEEG_BAD_TYPE" == "" ]]; then
+    export OPENMEEG_BAD_TYPE=1
+fi
+if [[ "$OPENMEEG_BAD_MSVC" == "" ]]; then
+    export OPENMEEG_BAD_MSVC=$(python -c "import sys; print(int(sys.platform=='win32'))")
+fi
 set -xe
 pytest $(python -c 'from pathlib import Path; import openmeeg; print(Path(openmeeg.__file__).parent)')
