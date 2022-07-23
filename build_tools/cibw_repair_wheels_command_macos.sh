32bca8f7b7a527333c97fa0bbb4a0750ce69aa81
Eric Larson <larson.eric.d@gmail.com>
2022-07-23 18:26:59 -0500

    FIX: Streamline

---
+++ build_tools/cibw_repair_wheels_command_macos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+if [[ "$1" == "" ]] || [[ "$2" == ""]] || [[ "$3" == "" ]]; then
+    echo "Usage: $0 <DELOCATE_ARCHS> <DEST_DIR> <WHEEL>"
+    exit 1
+fi
+DELOCATE_ARCHS=$1
+DEST_DIR=$2
+WHEEL=$3
+set -x
+
+export DYLD_LIBRARY_PATH="$GITHUB_WORKSPACE/install/lib"
+delocate-listdeps "$WHEEL"
+delocate-wheel --require-archs "$DELOCATE_ARCHS" -w "$DEST_DIR" "$WHEEL"
