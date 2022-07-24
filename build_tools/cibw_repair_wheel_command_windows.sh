65250140d8fd7e8dfe8efd89d3580a4078ee69f4
Eric Larson <larson.eric.d@gmail.com>
2022-07-23 22:17:40 -0400

    FIX: No mangle

--- build_tools/cibw_repair_wheel_command_windows.sh
+++ build_tools/cibw_repair_wheel_command_windows.sh
@@ -14,4 +14,4 @@ ADD_PATH=$(cygpath -w $ADD_PATH)
 echo "Adding path \"$ADD_PATH\""
 set -x
 delvewheel show --add-path="$ADD_PATH" "$WHEEL"
-delvewheel repair --add-path="$ADD_PATH" -w "$DEST_DIR" "$WHEEL"
+delvewheel repair --add-path="$ADD_PATH" --no-mangle="libquadmath-0.dll;libgcc_s_seh-1.dll" -w "$DEST_DIR" "$WHEEL"
