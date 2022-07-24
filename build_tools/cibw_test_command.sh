5939430b13ab1d32dd4049e276774613399c1369
Eric Larson <larson.eric.d@gmail.com>
2022-07-24 08:58:58 -0400

    MAINT: Reenable pypy wheels

--- build_tools/cibw_test_command.sh
+++ build_tools/cibw_test_command.sh
@@ -2,6 +2,8 @@
 
 if [[ "$1" != "" ]]; then
     export OPENMEEG_DATA_PATH=$1/data
+elif [[ "$OPENMEEG_DATA_PATH" == "" ]] && [[ -f "$PWD/data/Head1/Head1.geom" ]]; then
+    export OPENMEEG_DATA_PATH=$PWD/data
 fi
 # TODO: We need to reenable the tests that this comments out. Something about
 # how cmake SWIG wraps is better than how setuptools does it
@@ -11,5 +13,7 @@ fi
 if [[ "$OPENMEEG_BAD_MSVC" == "" ]]; then
     export OPENMEEG_BAD_MSVC=$(python -c "import sys; print(int(sys.platform=='win32'))")
 fi
+TEST_PATH=$(python -c 'from pathlib import Path; import openmeeg; print(Path(openmeeg.__file__).parent)')
+echo "OPENMEEG_DATA_PATH=\"$OPENMEEG_DATA_PATH\""
 set -xe
-pytest $(python -c 'from pathlib import Path; import openmeeg; print(Path(openmeeg.__file__).parent)')
+pytest -rav "$TEST_PATH"
