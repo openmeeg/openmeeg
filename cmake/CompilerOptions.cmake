afbdacb19ca909e07a39e325a9d86ff41ee4cc31
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2022-08-04 17:13:07 +0200

    Minor improvement.

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -2,7 +2,6 @@
 # Test and setup the C++ compiler features
 #---------------------------------------------------------------
 
-include(CheckCXXFeatures)
 option(USE_OPENMP "Use OpenMP (if available)"  ON)
 
 if(CMAKE_CXX_COMPILER_LOADED)
@@ -13,6 +12,7 @@ if(CMAKE_CXX_COMPILER_LOADED)
 endif()
 
 if (USE_OPENMP)
+    include(CheckCXXFeatures)
     find_package(OpenMP)
     if (OpenMP_FOUND)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
