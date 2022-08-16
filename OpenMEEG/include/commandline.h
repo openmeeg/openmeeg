ec90418aa0df7d6787f07024c84479a4b55ca512
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2022-08-16 15:26:53 +0200

    Remove no longer useful pragmas.

--- OpenMEEG/include/commandline.h
+++ OpenMEEG/include/commandline.h
@@ -46,11 +46,7 @@ knowledge of the CeCILL-B license and that you accept its terms.
 #include <sstream>
 #include <initializer_list>
 
-#ifdef WIN32
-#pragma warning( disable : 4530)    //MSVC standard library can't be inlined
-#pragma warning( disable : 4996)    //MSVC warning C4996: declared deprecated
-#pragma warning( disable : 4290)    //MSVC warning C4290: C++ exception specification
-#else
+#ifndef WIN32
 #define use_color_terminal
 #endif
 
