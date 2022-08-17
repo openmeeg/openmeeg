88b8ff1c9f4e3b2e0631a91f479ebd72caa1120c
Eric Larson <larson.eric.d@gmail.com>
2022-08-17 11:09:36 -0400

    MAINT: Correct code for black and flake8 (#554)
    MAINT: Use black and pyflakes

--- wrapping/python/openmeeg/tests/test_doc.py
+++ wrapping/python/openmeeg/tests/test_doc.py
@@ -7,6 +7,9 @@ def test_doc():
     doc = inspect.getdoc(om.HeadMat)
     assert doc is not None
 
-    headmat_expected_docstring = \
-        ("HeadMat(Geometry geo, Integrator const & integrator=Integrator(3,0,0.005)) -> SymMatrix")
-    assert doc == headmat_expected_docstring, f'got: {repr(doc)} != expected: {repr(headmat_expected_docstring)}'
+    headmat_expected_docstring = """\
+HeadMat(Geometry geo, Integrator const & integrator=Integrator(3,0,0.005)) \
+-> SymMatrix"""
+    assert (
+        doc == headmat_expected_docstring
+    ), f"got: {repr(doc)} != expected: {repr(headmat_expected_docstring)}"
--- wrapping/python/setup.py
+++ wrapping/python/setup.py
@@ -14,22 +14,22 @@ from setuptools.command import build_py
 root = Path(__file__).parent
 
 version = None
-with open((root / 'openmeeg' / '_version.py'), 'r') as fid:
+with open((root / "openmeeg" / "_version.py"), "r") as fid:
     for line in (line.strip() for line in fid):
-        if line.startswith('__version__'):
-            version = line.split('=')[1].strip().strip('\'')
+        if line.startswith("__version__"):
+            version = line.split("=")[1].strip().strip('"')
             break
 if version is None:
-    raise RuntimeError('Could not determine version')
+    raise RuntimeError("Could not determine version")
 
 
-DISTNAME = 'openmeeg'
-DESCRIPTION = 'Forward problems solver in the field of EEG and MEG.'
-MAINTAINER = 'Alexandre Gramfort'
-MAINTAINER_EMAIL = 'alexandre.gramfort@inria.fr'
-URL = 'https://openmeeg.github.io/'
-LICENSE = 'CECILL-B'
-DOWNLOAD_URL = 'http://github.com/openmeeg/openmeeg'
+DISTNAME = "openmeeg"
+DESCRIPTION = "Forward problems solver in the field of EEG and MEG."
+MAINTAINER = "Alexandre Gramfort"
+MAINTAINER_EMAIL = "alexandre.gramfort@inria.fr"
+URL = "https://openmeeg.github.io/"
+LICENSE = "CECILL-B"
+DOWNLOAD_URL = "http://github.com/openmeeg/openmeeg"
 VERSION = version
 
 # Adapted from MIT-licensed
@@ -38,7 +38,6 @@ try:
     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
     class bdist_wheel(_bdist_wheel):
-
         def finalize_options(self):
             _bdist_wheel.finalize_options(self)
             # Mark us as not a pure python package
@@ -55,47 +54,47 @@ class BuildExtFirst(build_py.build_py):
         super().run()
 
 
-
 if __name__ == "__main__":
     import numpy as np
-    manifest = (root / 'MANIFEST')
+
+    manifest = root / "MANIFEST"
     if manifest.is_file():
         os.remove(manifest)
 
-    with open('README.rst', 'r') as fid:
+    with open("README.rst", "r") as fid:
         long_description = fid.read()
 
     # SWIG
     cmdclass = dict(build_py=BuildExtFirst)
     ext_modules = []
-    if os.getenv('OPENMEEG_USE_SWIG', '0').lower() in ('1', 'true'):
+    if os.getenv("OPENMEEG_USE_SWIG", "0").lower() in ("1", "true"):
         include_dirs = [np.get_include()]
-        swig_opts = ['-c++', '-v', '-O']  # TODO: , '-Werror']
+        swig_opts = ["-c++", "-v", "-O"]  # TODO: , '-Werror']
         library_dirs = []
-        openmeeg_include = os.getenv('OPENMEEG_INCLUDE')
+        openmeeg_include = os.getenv("OPENMEEG_INCLUDE")
         if openmeeg_include is not None:
             openmeeg_include = Path(openmeeg_include).resolve()
             assert openmeeg_include.is_dir(), openmeeg_include
             include_dirs.append(str(openmeeg_include))
-            swig_opts.append(f'-I{openmeeg_include}')
-        msvc = os.getenv('SWIG_FLAGS', '') == 'msvc'
-        openblas_include = os.getenv('OPENBLAS_INCLUDE')
+            swig_opts.append(f"-I{openmeeg_include}")
+        msvc = os.getenv("SWIG_FLAGS", "") == "msvc"
+        openblas_include = os.getenv("OPENBLAS_INCLUDE")
         if openblas_include is not None:
             openblas_include = Path(openblas_include).resolve()
             assert openblas_include.is_dir(), openblas_include
             include_dirs.append(str(openblas_include))
-            swig_opts.append(f'-I{openblas_include}')
-        openmeeg_lib = os.getenv('OPENMEEG_LIB')
+            swig_opts.append(f"-I{openblas_include}")
+        openmeeg_lib = os.getenv("OPENMEEG_LIB")
         if openmeeg_lib is not None:
             openmeeg_lib = Path(openmeeg_lib).resolve()
             assert openmeeg_lib.is_dir(), openmeeg_lib
             library_dirs.append(str(openmeeg_lib))
         extra_compile_opts, extra_link_opts = [], []
         if msvc:
-            extra_compile_opts.extend(['/std:c++17'])
-            extra_link_opts.extend(['/std:c++17'])
+            extra_compile_opts.extend(["/std:c++17"])
+            extra_link_opts.extend(["/std:c++17"])
         else:
-            extra_compile_opts.extend(['-v', '-std=c++17'])
+            extra_compile_opts.extend(["-v", "-std=c++17"])
         # An example cmake command that works is:
         #   C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.16.27023\bin\HostX64\x64\link.exe
         #     /ERRORREPORT:QUEUE /INCREMENTAL:NO /NOLOGO
@@ -110,11 +109,10 @@ if __name__ == "__main__":
         #     /c /nologo /O2 /W3 /GL /DNDEBUG /MD
         #     /EHsc /Tpopenmeeg/openmeeg_wrap.cpp /Fobuild\temp.win-amd64-cpython-310\Release\openmeeg/openmeeg_wrap.obj
 
-
         swig_openmeeg = Extension(
             "openmeeg._openmeeg",
             sources=["openmeeg/openmeeg.i"],
-            libraries=['OpenMEEG'],
+            libraries=["OpenMEEG"],
             swig_opts=swig_opts,
             extra_compile_args=extra_compile_opts,
             include_dirs=include_dirs,
@@ -123,43 +121,45 @@ if __name__ == "__main__":
         ext_modules.append(swig_openmeeg)
     else:  # built with -DENABLE_PYTHON=ON
         # TODO: This breaks macOS for some reason!
-        if sys.platform != 'darwin':
-            cmdclass['bdist_wheel'] = bdist_wheel
+        if sys.platform != "darwin":
+            cmdclass["bdist_wheel"] = bdist_wheel
 
-    setup(name=DISTNAME,
-          maintainer=MAINTAINER,
-          include_package_data=True,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          license=LICENSE,
-          url=URL,
-          version=VERSION,
-          download_url=DOWNLOAD_URL,
-          long_description=long_description,
-          long_description_content_type='text/x-rst',
-          zip_safe=False,  # the package can run out of an .egg file
-          classifiers=['Intended Audience :: Science/Research',
-                       'Intended Audience :: Developers',
-                       'License :: OSI Approved',
-                       'Programming Language :: Python',
-                       'Topic :: Software Development',
-                       'Topic :: Scientific/Engineering',
-                       'Operating System :: Microsoft :: Windows',
-                       'Operating System :: POSIX',
-                       'Operating System :: Unix',
-                       'Operating System :: MacOS',
-                       'Programming Language :: Python :: 3',
-                       ],
-          keywords='neuroscience neuroimaging MEG EEG ECoG sEEG iEEG brain',
-          project_urls={
-              'Documentation': 'https://openmeeg.github.io',
-              'Source': 'https://github.com/openmeeg/openmeeg',
-              'Tracker': 'https://github.com/openmeeg/openmeeg/issues',
-          },
-          platforms='any',
-          python_requires='>=3.7',
-          install_requires=["numpy"],
-          packages=["openmeeg", "openmeeg.tests"],
-          cmdclass=cmdclass,
-          ext_modules=ext_modules,
-          )
+    setup(
+        name=DISTNAME,
+        maintainer=MAINTAINER,
+        include_package_data=True,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        version=VERSION,
+        download_url=DOWNLOAD_URL,
+        long_description=long_description,
+        long_description_content_type="text/x-rst",
+        zip_safe=False,  # the package can run out of an .egg file
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved",
+            "Programming Language :: Python",
+            "Topic :: Software Development",
+            "Topic :: Scientific/Engineering",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+            "Programming Language :: Python :: 3",
+        ],
+        keywords="neuroscience neuroimaging MEG EEG ECoG sEEG iEEG brain",
+        project_urls={
+            "Documentation": "https://openmeeg.github.io",
+            "Source": "https://github.com/openmeeg/openmeeg",
+            "Tracker": "https://github.com/openmeeg/openmeeg/issues",
+        },
+        platforms="any",
+        python_requires=">=3.7",
+        install_requires=["numpy"],
+        packages=["openmeeg", "openmeeg.tests"],
+        cmdclass=cmdclass,
+        ext_modules=ext_modules,
+    )
