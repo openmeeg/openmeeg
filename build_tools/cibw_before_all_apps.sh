bc55a102748907dc87aca575170947151e2fe3ea
Eric Larson <larson.eric.d@gmail.com>
2022-08-25 11:21:46 +0100

    FIX: Try again

--- build_tools/cibw_before_all_apps.sh
+++ build_tools/cibw_before_all_apps.sh
@@ -34,31 +34,33 @@ echo "Using NumPy PLATFORM=\"${PLATFORM}\""
 # win-amd64
 
 if [[ "$PLATFORM" == "linux-x86_64" ]]; then
-    dnf -y install epel-release
-    dnf -y install hdf5-devel matio-devel
+    dnf -y install curl zip unzip tar
     export OPENBLAS_INCLUDE=/usr/local/include
     export OPENBLAS_LIB=/usr/local/lib
     export CMAKE_CXX_FLAGS="-I$OPENBLAS_INCLUDE"
     export LINKER_OPT="-lgfortran -lpthread"
+    export VCPKG_DEFAULT_TRIPLET="x64-linux"
+    source ./build_tools/setup_vcpkg_compilation.sh
+    LAPACK_LIBRARIES_OPT="-DLAPACK_LIBRARIES=/usr/local/lib/libopenblas.a"
     SHARED_OPT="-DBUILD_SHARED_LIBS=OFF"
 elif [[ "$PLATFORM" == 'macosx-'* ]]; then
-    brew install swig
+    # brew install swig libomp # This might be for macOS 11+!
     BLAS_DIR=/usr/local
     OPENBLAS_INCLUDE=$BLAS_DIR/include
     OPENBLAS_LIB=$BLAS_DIR/lib
     export CMAKE_CXX_FLAGS="-I$OPENBLAS_INCLUDE"
     export CMAKE_PREFIX_PATH="$BLAS_DIR"
-    export LINKER_OPT="-L$OPENBLAS_LIB"
+    export LINKER_OPT="-L$OPENBLAS_LIB -lgfortran"
     echo "Building for CIBW_ARCHS_MACOS=\"$CIBW_ARCHS_MACOS\""
     if [[ "$CIBW_ARCHS_MACOS" == "x86_64" ]]; then
         export VCPKG_DEFAULT_TRIPLET="x64-osx-release-10.9"
         source ./build_tools/setup_vcpkg_compilation.sh
-        export SYSTEM_VERSION_OPT="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15"
+        cp -av /usr/local/gfortran/lib/libgfortran* $OPENBLAS_LIB/
     elif [[ "$CIBW_ARCHS_MACOS" == "arm64" ]]; then
         # export VCPKG_DEFAULT_TRIPLET="arm64-osx-release-10.9"
         CMAKE_OSX_ARCH_OPT="-DCMAKE_OSX_ARCHITECTURES=arm64"
         # The deps were compiled locally on 2022/07/19 on an M1 machine and uploaded
-        curl -L https://osf.io/download/x45fz > openmeeg-deps-arm64-osx-release-10.9.tar.gz
+        curl -L https://osf.io/download/x45fz?version=1 > openmeeg-deps-arm64-osx-release-10.9.tar.gz
         tar xzfv openmeeg-deps-arm64-osx-release-10.9.tar.gz
         CMAKE_PREFIX_PATH_OPT="-DCMAKE_PREFIX_PATH=$ROOT/vcpkg_installed/arm64-osx-release-10.9"
         ls -al $ROOT/vcpkg_installed/arm64-osx-release-10.9/lib
@@ -66,24 +68,25 @@ elif [[ "$PLATFORM" == 'macosx-'* ]]; then
         # And downloading method taken from https://stackoverflow.com/a/69858397
         curl -LH "Authorization: Bearer QQ==" -o x.tar.gz https://ghcr.io/v2/homebrew/core/libomp/blobs/sha256:f00a5f352167b2fd68ad25b1959ef66a346023c6dbeb50892b386381d7ebe183
         tar xzfv x.tar.gz
-        cp -a libomp/14.0.6/lib/* $ROOT/vcpkg_installed/arm64-osx-release-10.9/lib/
-        cp -a libomp/14.0.6/include/* $ROOT/vcpkg_installed/arm64-osx-release-10.9/include/
+        VCPKG_DIR=$ROOT/vcpkg_installed/arm64-osx-release-10.9
+        cp -av libomp/14.0.6/lib/* $VCPKG_DIR/lib/
+        cp -a libomp/14.0.6/include/* $VCPKG_DIR/include/
+        cp -av /opt/gfortran-darwin-arm64/lib/gcc/arm64-apple-darwin20.0.0/10.2.1/libgfortran* $VCPKG_DIR/lib/
         export LINKER_OPT="$LINKER_OPT -L$ROOT/vcpkg_installed/arm64-osx-release-10.9/lib -lz"
-        export SYSTEM_VERSION_OPT="-DCMAKE_OSX_DEPLOYMENT_TARGET=11"
     else
         echo "Unknown CIBW_ARCHS_MACOS=\"$CIBW_ARCHS_MACOS\""
         exit 1
     fi
     CMAKE_OSX_ARCH_OPT="-DCMAKE_OSX_ARCHITECTURES=${CIBW_ARCHS_MACOS}"
-    # libomp can cause segfaults on macos... maybe from version conflicts with OpenBLAS, or from being too recent?
-    OPENMP_OPT="-DUSE_OPENMP=OFF"
+    OPENMP_STATIC_OPT="-DOPENMP_STATIC=ON"
 elif [[ "$PLATFORM" == "win-amd64" ]]; then
     export VCPKG_DEFAULT_TRIPLET="x64-windows-release-static"
     export CMAKE_GENERATOR="Visual Studio 16 2019"
     source ./build_tools/setup_vcpkg_compilation.sh
     source ./build_tools/download_openblas.sh windows  # NumPy doesn't install the headers for Windows
     pip install delvewheel
-    export SYSTEM_VERSION_OPT="-DCMAKE_SYSTEM_VERSION=7"
+    SYSTEM_VERSION_OPT="-DCMAKE_SYSTEM_VERSION=7"
+    OPENMP_STATIC_OPT="-DOPENMP_STATIC=ON"
 else
     echo "Unknown platform: ${PLATFORM}"
     exit 1
@@ -92,20 +95,24 @@ export PYTHON_OPT="-DENABLE_PYTHON=OFF"
 export BLA_IMPLEMENTATION="OpenBLAS"
 export DISABLE_CCACHE=1
 export WERROR_OPT="-DENABLE_WERROR=ON"
+export BLA_STATIC_OPT="-DBLA_STATIC=ON"
 pip install cmake
-./build_tools/cmake_configure.sh -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_INSTALL_PREFIX=${ROOT}/install ${OPENMP_OPT} ${CMAKE_OSX_ARCH_OPT} ${CMAKE_PREFIX_PATH_OPT} -DENABLE_APPS=OFF ${SHARED_OPT} -DCMAKE_INSTALL_UCRT_LIBRARIES=TRUE ${BLAS_LIBRARIES_OPT} ${LAPACK_LIBRARIES_OPT}
+./build_tools/cmake_configure.sh -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_INSTALL_PREFIX=${ROOT}/install ${SYSTEM_VERSION_OPT} ${CMAKE_OSX_ARCH_OPT} ${CMAKE_PREFIX_PATH_OPT} -DENABLE_APPS=ON ${SHARED_OPT} -DCMAKE_INSTALL_UCRT_LIBRARIES=FALSE ${OPENMP_STATIC_OPT} ${LAPACK_LIBRARIES_OPT}
 cmake --build build --target install --target package --config release
 
 # Put DLLs where they can be found
 if [[ "$PLATFORM" == 'linux'* ]]; then
     ls -al install/lib64/*.so*
     cp -av install/lib64/*.so* /usr/local/lib/
+    ls -alR /usr/local/lib
 elif [[ "$PLATFORM" == 'macosx-arm64' ]]; then
+    cp -av $ROOT/vcpkg_installed/arm64-osx-release-10.9/lib/libomp* $ROOT/install/lib/
+    cp -av $ROOT/vcpkg_installed/arm64-osx-release-10.9/lib/libgfortran* $ROOT/install/lib/
     # https://matthew-brett.github.io/docosx/mac_runtime_link.html
-    #cp -av $ROOT/vcpkg_installed/arm64-osx-release-10.9/lib/libomp* $ROOT/install/lib/
     otool -L $ROOT/install/lib/libOpenMEEG.1.1.0.dylib
-    # install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/libomp/lib/libomp.dylib" "@loader_path/libomp.dylib" $ROOT/install/lib/libOpenMEEG.1.1.0.dylib
-    # otool -L $ROOT/install/lib/libOpenMEEG.1.1.0.dylib
+    install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/libomp/lib/libomp.dylib" "@loader_path/libomp.dylib" $ROOT/install/lib/libOpenMEEG.1.1.0.dylib
+    install_name_tool -change "@rpath/libgfortran.5.dylib" "@loader_path/libomp.dylib" $ROOT/install/lib/libOpenMEEG.1.1.0.dylib
+    otool -L $ROOT/install/lib/libOpenMEEG.1.1.0.dylib
 elif [[ "$PLATFORM" == 'win'* ]]; then
     cp -av $OPENBLAS_LIB/libopenblas_v0.3.20-140-gbfd9c1b5-gcc_8_1_0.dll install/bin/
 fi
