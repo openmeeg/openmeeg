32ad1cc95ebea086e926eae926afa76fba901739
Eric Larson <larson.eric.d@gmail.com>
2022-08-26 17:48:34 +0100

    FIX: Perms

--- build_tools/cibw_before_all_apps.sh
+++ build_tools/cibw_before_all_apps.sh
@@ -74,7 +74,7 @@ elif [[ "$PLATFORM" == 'macosx-'* ]]; then
         curl -LH "Authorization: Bearer QQ==" -o x.tar.gz https://ghcr.io/v2/homebrew/core/libomp/blobs/sha256:f00a5f352167b2fd68ad25b1959ef66a346023c6dbeb50892b386381d7ebe183
         tar xzfv x.tar.gz
         VCPKG_DIR=$ROOT/vcpkg_installed/arm64-osx-release-10.9
-        chmod +w /opt/gfortran-darwin-arm64/lib/gcc/arm64-apple-darwin20.0.0/10.2.1/
+        chmod -R a+w /opt/gfortran-darwin-arm64/lib/gcc/arm64-apple-darwin20.0.0/10.2.1/
         cp -av /opt/gfortran-darwin-arm64/lib/gcc/arm64-apple-darwin20.0.0/10.2.1/libgfortran* $VCPKG_DIR/lib/
         export LINKER_OPT="$LINKER_OPT -L$ROOT/vcpkg_installed/arm64-osx-release-10.9/lib -lz"
         export SYSTEM_VERSION_OPT="-DCMAKE_OSX_DEPLOYMENT_TARGET=11"
