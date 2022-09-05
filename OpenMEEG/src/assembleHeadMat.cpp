f110cbd2b78c0d0b036d8a1d82e9f209ab4e7214
Eric Larson <larson.eric.d@gmail.com>
2022-09-04 20:13:23 -0400

    FIX: Specific

--- OpenMEEG/src/assembleHeadMat.cpp
+++ OpenMEEG/src/assembleHeadMat.cpp
@@ -83,8 +83,8 @@ namespace OpenMEEG {
             for (const auto& mp : geo.communicating_mesh_pairs()) {
                 const Mesh& mesh1 = mp(0);
                 const Mesh& mesh2 = mp(1);
-                mesh1.check_consistency("HeadMatrix->geo.communicating_mesh_pairs()");
-                mesh2.check_consistency("HeadMatrix->geo.communicating_mesh_pairs()");
+                mesh1.check_consistency("HeadMatrix->geo.communicating_mesh_pairs() mesh1");
+                mesh2.check_consistency("HeadMatrix->geo.communicating_mesh_pairs() mesh2");
                 if (verbose)
                     std::cout << "    Assembling " << mesh1.name() << " x " << mesh2.name();
 
