073c9132c0c5caeabc7c16c2c5d1cfd64acee3b3
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2022-09-05 11:42:23 +0200

    Remove a flake warning.

--- wrapping/python/openmeeg/__init__.py
+++ wrapping/python/openmeeg/__init__.py
@@ -3,4 +3,4 @@ from .openmeeg import *  # noqa
 from ._version import __version__
 from ._make_geometry import make_geometry, make_nested_geometry
 
-Logger.logger().set_info_level(WARNING)
+.openmeeg.Logger.logger().set_info_level(.openmeeg.WARNING)
