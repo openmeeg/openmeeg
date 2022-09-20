3f76c35cc8086e96b0a6ab3c2690f1132ef8f977
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2022-09-20 19:03:24 +0200

    Correct some mistakes.

--- wrapping/python/apps/minverser.py
+++ wrapping/python/apps/minverser.py
@@ -5,7 +5,7 @@ import argparse
 import pathlib
 import openmeeg as om
 
-def parser(argv):
+def parse_arguments(argv):
     parser = argparse.ArgumentParser(description='Compute the inverse of an head matrix.')
     parser.add_argument('head_matrix', type=open, nargs=1, help='head matrix file name')
     parser.add_argument('dest_file', type=pathlib.Path, nargs=1, help='output: inverse head matrix file name')
@@ -18,4 +18,4 @@ def parser(argv):
 def invert(args):
     head_matrix = om.SymMatrix(args.head_matrix)
     head_matrix.invert()
-    head_matrix.save(args.dest_file[0].name))
+    head_matrix.save(args.dest_file)
