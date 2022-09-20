086f9b8790130a168e3198ca5433db4a2a741eec
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2022-09-20 19:23:12 +0200

    Add a comprehensive list of public symbols.

--- wrapping/python/openmeeg/__init__.py
+++ wrapping/python/openmeeg/__init__.py
@@ -2,25 +2,39 @@ from . import _distributor_init
 
 # Here we import as few things as possible to keep our API as limited as
 # possible
+
 from ._openmeeg_cxx import (
+    Matrix,
+    SymMatrix,
+    SparseMatrix,
+
     HeadMat,
     Sensors,
-    Integrator,
-    Head2EEGMat,
-    Head2MEGMat,
-    DipSourceMat,
+
+    CorticalMat,
+    CorticalMat2,
+    DipSource2InternalPotMat,
     DipSource2MEGMat,
+    DipSourceMat,
+    EITSourceMat,
+    Forward,
     GainEEG,
     GainMEG,
     GainEEGadjoint,
     GainMEGadjoint,
     GainEEGMEGadjoint,
-    Forward,
+    GainEITInternalPot,
+    GainInternalPot,
+    Head2ECoGMat,
+    Head2EEGMat,
+    Head2MEGMat,
     SurfSourceMat,
     SurfSource2MEGMat,
-    Matrix,
-    SymMatrix,
+    Surf2VolMat,
+
+    Integrator,
 )
+
 from ._version import __version__
 from ._make_geometry import make_geometry, make_nested_geometry, read_geometry
 from ._utils import get_log_level, set_log_level, use_log_level
