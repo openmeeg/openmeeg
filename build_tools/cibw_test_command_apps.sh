2bef9534f99dc439ebc587ec73f01714daca1131
Eric Larson <larson.eric.d@gmail.com>
2023-11-15 17:37:38 -0500

    FIX: Try

--- build_tools/cibw_test_command_apps.sh
+++ build_tools/cibw_test_command_apps.sh
@@ -22,17 +22,17 @@ echo
 if [[ "${RUNNER_OS}" == "Linux" ]]; then
     tar xzfv $ROOT/installers/OpenMEEG-*-*.tar.gz
     cd OpenMEEG-*
+    ldd -v ./lib/libOpenMEEGMaths.so
     ldd ./lib/libOpenMEEG.so
-    ldd ./lib/libOpenMEEGMaths.so
     # readelf -d binary-or-library | head -20
     ./bin/om_minverser --help
 elif [[ "${RUNNER_OS}" == "macOS" ]]; then
     tar xzfv $ROOT/installers/OpenMEEG-*-*.tar.gz
     cd OpenMEEG-*
     sudo rm /usr/local/gfortran/lib/*.dylib
-    otool -L ./lib/libOpenMEEG.dylib
-    otool -L ./lib/libOpenMEEGMaths.dylib
     otool -L ./lib/libgfortran.*.dylib
+    otool -L ./lib/libOpenMEEGMaths.dylib
+    otool -L ./lib/libOpenMEEG.dylib
     ./bin/om_minverser --help
 elif [[ "${RUNNER_OS}" == "Windows" ]]; then
     ROOT=$(cygpath -u $ROOT)
