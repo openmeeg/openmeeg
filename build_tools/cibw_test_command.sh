8a262a3c0a596c95fcf859dd8f748ae69f668bf9
Eric Larson <larson.eric.d@gmail.com>
2024-01-29 12:40:46 -0500

    FIX: Path

--- build_tools/cibw_test_command.sh
+++ build_tools/cibw_test_command.sh
@@ -12,4 +12,5 @@ echo "OPENMEEG_DATA_PATH=\"$OPENMEEG_DATA_PATH\""
 echo
 set -xe
 python -m threadpoolctl -i numpy openmeeg
+ls -al $TEST_PATH/tests
 pytest --tb=short -ra -m "not slow" -vv "$TEST_PATH"
