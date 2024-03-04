75a6806dc1186b3b73d11c9bc4e68cdd821460c1
Eric Larson <larson.eric.d@gmail.com>
2024-03-04 12:03:09 -0500

    FIX: Simplify

--- build_tools/cibw_before_build.sh
+++ build_tools/cibw_before_build.sh
@@ -11,9 +11,5 @@ cd $ROOT
 pwd
 
 IS_PYPY=$(python -c "import sys; print(int('pypy' in sys.implementation.name))")
-if [[ "$IS_PYPY" == "1" ]]; then
-    python -m pip install numpy --only-binary="numpy"
-else
-    python -m pip install --upgrade --pre --only-binary="numpy" --extra-index-url="https://anaconda.org/scientific-python-nightly-wheels/simple" "numpy>=2.0.0.dev0"
-fi
+python -m pip install --upgrade --pre --only-binary="numpy" --extra-index-url="https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" "numpy>=2.0.0.dev0"
 pip install "setuptools>=68.0.0" "wheel>=0.37.0"
--- build_tools/cibw_before_build_windows.sh
+++ build_tools/cibw_before_build_windows.sh
@@ -16,12 +16,7 @@ rm -Rf build
 cp -a build_nopython build
 which python
 python --version
-IS_PYPY=$(python -c "import sys; print(int('pypy' in sys.implementation.name))")
-if [[ "$IS_PYPY" == "1" ]]; then
-    python -m pip install numpy --only-binary="numpy"
-else
-    python -m pip install --upgrade --pre --only-binary="numpy" --extra-index-url="https://anaconda.org/scientific-python-nightly-wheels/simple" "numpy>=2.0.0.dev0"
-fi
+python -m pip install --upgrade --pre --only-binary="numpy" --extra-index-url="https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" "numpy>=2.0.0.dev0"
 python -m pip install "setuptools>=68.0.0" "wheel>=0.37.0"
 cmake -B build -DENABLE_PYTHON=ON -DPython3_EXECUTABLE="$(which python)" .
 cmake --build build --config Release
