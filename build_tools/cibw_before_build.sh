f6d73ea9c661c71f1fa06ed3552c93d1e248321e
Eric Larson <larson.eric.d@gmail.com>
2024-03-04 11:27:24 -0500

    MAINT: Ensure compat with NumPy 2.0 on PyPI

--- build_tools/cibw_before_build.sh
+++ build_tools/cibw_before_build.sh
@@ -10,21 +10,10 @@ echo "Using project root \"${ROOT}\" on RUNNER_OS=\"${RUNNER_OS}\""
 cd $ROOT
 pwd
 
-# Build the Python bindings on Windows
-ls -al
-rm -Rf build
-cp -a build_nopython build
-which python
-python --version
 IS_PYPY=$(python -c "import sys; print(int('pypy' in sys.implementation.name))")
 if [[ "$IS_PYPY" == "1" ]]; then
     python -m pip install numpy --only-binary="numpy"
 else
     python -m pip install --upgrade --pre --only-binary="numpy" --extra-index-url="https://anaconda.org/scientific-python-nightly-wheels/simple" "numpy>=2.0.0dev0"
 fi
-python -m pip install "setuptools>=68.0.0" "wheel>=0.37.0"
-cmake -B build -DENABLE_PYTHON=ON -DPython3_EXECUTABLE="$(which python)" .
-cmake --build build --config Release
-python -m pip uninstall -yq numpy
-cp -av build/wrapping/python/openmeeg/*.pyd build/wrapping/python/openmeeg/_openmeeg_wrapper.py wrapping/python/openmeeg/
-rm -Rf build
+pip install "setuptools>=68.0.0" "wheel>=0.37.0"
