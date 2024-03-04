3065421ab2fa6fded156bd311334fb003aee7463
Eric Larson <larson.eric.d@gmail.com>
2024-03-04 15:04:13 -0500

    MAINT: Ensure compat with NumPy 2.0 on PyPI (#639)

---
+++ build_tools/cibw_before_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+if [[ "$1" == "" ]]; then
+    echo "Usage: $0 <PROJECT_PATH>"
+    exit 1
+fi
+ROOT=$1
+echo "Using project root \"${ROOT}\" on RUNNER_OS=\"${RUNNER_OS}\""
+cd $ROOT
+pwd
+
+IS_PYPY=$(python -c "import sys; print(int('pypy' in sys.implementation.name))")
+python -m pip install --upgrade --pre --only-binary="numpy" --extra-index-url="https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" "numpy>=2.0.0.dev0"
+pip install "setuptools>=68.0.0" "wheel>=0.37.0"
--- build_tools/cibw_before_build_windows.sh
+++ build_tools/cibw_before_build_windows.sh
@@ -16,15 +16,9 @@ rm -Rf build
 cp -a build_nopython build
 which python
 python --version
-IS_PYPY=$(python -c "import sys; print(int('pypy' in sys.implementation.name))")
-if [[ "$IS_PYPY" == "1" ]]; then
-    NUMPY_PIP="numpy==1.26.3"
-else
-    NUMPY_PIP="oldest-supported-numpy"
-fi
-python -m pip install "$NUMPY_PIP" --only-binary="numpy"
+python -m pip install --upgrade --pre --only-binary="numpy" --extra-index-url="https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" "numpy>=2.0.0.dev0"
+python -m pip install "setuptools>=68.0.0" "wheel>=0.37.0"
 cmake -B build -DENABLE_PYTHON=ON -DPython3_EXECUTABLE="$(which python)" .
 cmake --build build --config Release
-python -m pip uninstall -yq numpy
 cp -av build/wrapping/python/openmeeg/*.pyd build/wrapping/python/openmeeg/_openmeeg_wrapper.py wrapping/python/openmeeg/
 rm -Rf build
