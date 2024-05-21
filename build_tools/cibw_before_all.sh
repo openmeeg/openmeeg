97dde31ef6720ff5aae366426ac721a3eaafa0df
Eric Larson <larson.eric.d@gmail.com>
2024-05-21 12:58:48 -0400

    FIX: Perms

--- build_tools/cibw_before_all.sh
+++ build_tools/cibw_before_all.sh
@@ -15,7 +15,8 @@ echo "Using project root \"${ROOT}\" on RUNNER_OS=\"${RUNNER_OS}\""
 # Let's have NumPy help us out
 if [[ "$RUNNER_OS" == "macOS" ]] && [[ $(uname -m) == 'arm64' ]]; then
     echo "Making /usr/local/lib for macOS arm64"
-    mkdir -p /usr/local/lib
+    sudo mkdir -p /usr/local/lib
+    sudo chown $USER /usr/local/lib
 fi
 curl -L https://github.com/numpy/numpy/archive/refs/tags/v1.23.1.tar.gz | tar xz numpy-1.23.1
 mv numpy-1.23.1/tools .
