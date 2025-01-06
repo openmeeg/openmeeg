7dec73615a7f06f894c7dbccfbfa8fc8e9849074
Eric Larson <larson.eric.d@gmail.com>
2025-01-06 14:19:48 -0500

    FIX: Better

--- build_tools/download_openblas.sh
+++ build_tools/download_openblas.sh
@@ -20,7 +20,7 @@ fi
 echo "Downloading OpenBLAS and setting cmake flags for PLATFORM=\"${PLATFORM}\""
 curl -LO https://anaconda.org/multibuild-wheels-staging/openblas-libs/${BLAS_VER}/download/${BLAS_FILENAME}
 if [[ "${PLATFORM}" == 'win'* ]]; then
-    unzip ${BLAS_FILENAME} -fod openblas
+    unzip -f -o -d openblas ${BLAS_FILENAME}
     export OPENBLAS_LIB=${PWD}/openblas/64/lib
     export OPENBLAS_INCLUDE=${PWD}/openblas/64/include
     BLAS_EXT="${BLAS_VER}-gcc_8_1_0"
