feb592a1ba3c45c0a46a64cfb820316d888a87f5
Eric Larson <larson.eric.d@gmail.com>
2025-01-07 11:00:38 -0500

    TST: Test pytest

--- build_tools/cibw_test_command.sh
+++ build_tools/cibw_test_command.sh
@@ -12,3 +12,7 @@ pwd
 pytest --tb=short -ra -m "not slow" -vv --pyargs openmeeg
 # Smoke test for https://github.com/swig/swig/issues/3061
 PYTHONFAULTHANDLER=1 PYTHONWARNINGS=error python -uc "import openmeeg"
+
+# Rerun the "bad" way
+TEST_PATH=$(python -c 'from pathlib import Path; import openmeeg; print(Path(openmeeg.__file__).parent)')￼
+pytest --tb=short -ra -m "not slow" -vv "$TEST_PATH"
